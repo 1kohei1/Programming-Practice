326665554da3987d48a59faaf4fd342f82bf32b2
Kohei Arai <tobecomebig@gmail.com>
2016-05-06 22:51:42 -0400

    uva 11286

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,55 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.TreeSet;
 
-// uva 10920
+// uva 11286
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(in.readLine());
 		
-		while(true) {
-			int n = in.nextInt();
-			if (n == 0) {
-				break;
+		while (n != 0) {
+			HashMap<TreeSet<Integer>, Integer> map = new HashMap<TreeSet<Integer>, Integer>();
+			
+			int[] answer = new int[n + 1];
+			
+			for (int i = 0; i < n; i++) {
+				String s = in.readLine();
+				String[] sArray = s.split(" ");
+				
+				TreeSet<Integer> nums = new TreeSet<Integer>();
+				for (int j = 0; j < 5; j++) {
+					nums.add(Integer.parseInt(sArray[j]));
+				}
+				
+				if (map.containsKey(nums)) {
+					int prePopularity = map.get(nums);
+					answer[prePopularity] -= prePopularity;
+					
+					int popularity = prePopularity + 1;
+					
+					map.put(nums, popularity);
+					answer[popularity] += popularity;
+				} else {
+					map.put(nums, 1);
+					answer[1]++;
+				}
 			}
-			int p = in.nextInt();
 			
-			if (p == 1) {
-				printAnswer((n + 1) / 2, (n + 1) / 2);
-				continue;
-			}
-			
-			int num = 0;
-			boolean answerFound = false;
-			
-			for (int i = 1; i <= n; i += 2) {
-				if (i * i == p) {
-					int a = (n + 1) / 2 + i / 2;
-					printAnswer(a, a);
-					answerFound = true;
-				} else if (p < i * i) {
-					num = i - 2;
+			for (int i = n; i >= 0; i--) {
+				if (answer[i] != 0) {
+					System.out.println(answer[i]);
 					break;
 				}
 			}
-//			System.out.println("num: " + num);
 			
-			if (answerFound) {
-				continue;
-			}
-			
-			// If it's in row above
-			if (num * num < p && p <= num * num + (1 + num)) {
-				int diff = num * num + 1 - p;
-				int row = (n + 1) / 2 + num / 2 + 1;
-				int col = (n + 1) / 2 + num / 2 + diff;
-				printAnswer(row, col);
-			}
-			// If it's in left column
-			else if (num * num + (1 + num) < p && p <= num * num + 2 * (1 + num)) {
-				int diff = num * num + (1 + num) - p;
-				int row = (n + 1) / 2 + num / 2 + 1 + diff;
-				int col = (n + 1) / 2 + num / 2 - num;
-				printAnswer(row, col);
-			}
-			// If it's in bottom row
-			else if (num * num + 2 * (1 + num) < p && p <= num * num + 3 * (1 + num)) {
-				int diff = num * num + 2 * (1 + num) - p;
-				int row = (n + 1) / 2 + num / 2 - num;
-				int col = (n + 1) / 2 + num / 2 - num - diff;
-				printAnswer(row, col);
-			} else {
-				int diff = (num + 2) * (num + 2) - p;
-				int row = (n + 1) / 2 + num / 2 + 1 - diff;
-				int col = (n + 1) / 2 + num / 2 + 1;
-				printAnswer(row, col);
-			}
+			n = Integer.parseInt(in.readLine());
 		}
 	}
 	
-	public static void printAnswer(int a, int b) {
-		System.out.printf("Line = %d, column = %d.\n", a, b);
-	}
 }
