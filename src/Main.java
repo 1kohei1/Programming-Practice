a571d4944fdc3222de12626cde2e9616f7d9683c
Kohei Arai <tobecomebig@gmail.com>
2016-05-14 17:01:24 -0400

    uva 11565

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.TreeSet;
 
-// uva 441
+// uva 11565
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
-		String[] s = in.readLine().split(" ");
-		int x = Integer.parseInt(s[0]);
-		
-		boolean isFirst = true;
-		
-		while (x != 0) {
+		int nt = Integer.parseInt(in.readLine());
+		for (int counter = 0; counter < nt; counter++) {
+			String[] s = in.readLine().split(" ");
+			int a = to(s[0]);
+			int b = to(s[1]);
+			int c = to(s[2]);
 			
-			if (!isFirst) {
-				System.out.println();
-			}
+			boolean solutionFound = false;
 			
-			int[] nums = new int[x];
-			for (int i = 0; i < x; i++) {
-				nums[i] = Integer.parseInt(s[i + 1]);
-	 		}
-
-			for (int i = 0; i <= x - 6; i++) {
-				for (int j = i + 1; j <= x - 5; j++) {
-					for (int k = j + 1; k <= x - 4; k++) {
-						for (int l = k + 1; l <= x - 3; l++) {
-							for (int m = l + 1; m <= x - 2; m++) {
-								for (int n = m + 1; n <= x - 1; n++) {
-									System.out.printf("%d %d %d %d %d %d\n", nums[i], nums[j], nums[k], nums[l], nums[m], nums[n]);
-								}
-							}
+			for (int x = -100; x <= 100 && !solutionFound; x++) {
+				for (int y = -100;y <= 100 && !solutionFound; y++) {
+					for (int z = -100;z <= 100 && !solutionFound; z++) {
+						if (x != y && y != z && x != z && x + y + z == a && x * y * z == b && x * x + y * y + z * z == c) {
+							solutionFound = true;
+							System.out.printf("%d %d %d\n", x, y, z);
 						}
 					}
 				}
 			}
 			
-			s = in.readLine().split(" ");
-			x = Integer.parseInt(s[0]);
-			isFirst = false;
+			if (!solutionFound) {
+				System.out.println("No solution.");
+			}
 		}
+	}
+	
+	public static int to(String s) {
+		return Integer.parseInt(s);
 	}
 }
