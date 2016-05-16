5627ec219acc47524d1e4164fd64def8c94b844f
Kohei Arai <tobecomebig@gmail.com>
2016-05-16 16:11:00 -0400

    uva 11742

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,109 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// uva 11565
+// uva 11742
 
 public class Main {
 
+	static int n, m;
+	static long answer;
+	static Constrain[] constrains;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
-		int nt = Integer.parseInt(in.readLine());
-		for (int counter = 0; counter < nt; counter++) {
-			String[] s = in.readLine().split(" ");
-			int a = to(s[0]);
-			int b = to(s[1]);
-			int c = to(s[2]);
+		String[] s;
+		
+		s = in.readLine().split(" ");
+		n = to(s[0]);
+		m = to(s[1]);
+		
+		while (n != 0 || m != 0) {
 			
-			boolean solutionFound = false;
+			answer = 0;
+			constrains = new Constrain[m];
 			
-			for (int x = -100; x <= 100 && !solutionFound; x++) {
-				for (int y = -100;y <= 100 && !solutionFound; y++) {
-					for (int z = -100;z <= 100 && !solutionFound; z++) {
-						if (x != y && y != z && x != z && x + y + z == a && x * y * z == b && x * x + y * y + z * z == c) {
-							solutionFound = true;
-							System.out.printf("%d %d %d\n", x, y, z);
-						}
-					}
-				}
+			for (int i = 0; i < m; i++) {
+				s = in.readLine().split(" ");
+				int a = to(s[0]);
+				int b = to(s[1]);
+				int dist = to(s[2]);
+				
+				int atMost = dist > 0 ? dist : n;
+				int atLeast = dist > 0 ? 0: -1 * dist;
+				
+				constrains[i] = new Constrain(a, b, atLeast, atMost);
 			}
 			
-			if (!solutionFound) {
-				System.out.println("No solution.");
+			solve(0, new boolean[n], new int[n]);
+			System.out.println(answer);
+			
+			s = in.readLine().split(" ");
+			n = to(s[0]);
+			m = to(s[1]);
+		}
+	}
+	
+	public static void solve(int index, boolean[] used, int[] perm) {
+		if (index == n) {
+//			printPerm(perm);
+			if (isValid(perm)) {
+				answer++;
+			}
+			return;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			if (used[i] == false) {
+				perm[index] = i;
+				used[i] = true;
+				solve(index + 1, used, perm);
+				used[i] = false;
 			}
 		}
 	}
 	
+	public static boolean isValid(int[] perm) {
+		for (int i = 0; i < m; i++) {
+			Constrain c = constrains[i];
+			int a = c.a;
+			int b = c.b;
+			int atLeast = c.atLeast;
+			int atMost = c.atMost;
+			
+			int dist = Math.abs(perm[a] - perm[b]);
+			if (atLeast <= dist && dist <= atMost) {
+				continue;
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void printPerm(int[] perm) {
+		for (int i = 0; i < n; i++) {
+			System.out.printf("%d ", perm[i]);
+		}
+		System.out.println();
+	}
+	
 	public static int to(String s) {
 		return Integer.parseInt(s);
+	}
+}
+
+class Constrain {
+	int a;
+	int b;
+	int atLeast;
+	int atMost;
+	
+	public Constrain(int a, int b, int atLeast, int atMost) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.atLeast = atLeast;
+		this.atMost = atMost;
 	}
 }
