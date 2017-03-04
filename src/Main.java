590b861c5fed442ea4fba23ffe44f8beb5aab1fc
Kohei Arai <tobecomebig@gmail.com>
2017-03-04 13:48:31 -0500

    uva 469

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,85 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-// uva 459
+// uva 469
 
 public class Main {
 	
-	static int[][] map;
-	static int[] visited;
+	static char[][] map;
+	static int[][] visited;
+	static int[] dx = new int[]{-1, 0, 1, -1, 1, -1, 0, 1};
+	static int[] dy = new int[]{-1, -1, -1, 0, 0, 1, 1, 1};
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
+	
 		int numTests = in.nextInt();
-		String s;
+		in.nextLine();
+		in.nextLine();
+		ArrayList<String> temp = new ArrayList<String>();
 		while (numTests-- > 0) {
-			int numNodes = in.next().charAt(0) - 'A' + 1;
-			in.nextLine();
-			visited = new int[numNodes];
-			map = new int[numNodes][numNodes];
+			temp.clear();
 			
-			s = in.nextLine();
-			while (s.length() != 0) {
-				int a = s.charAt(0) - 'A';
-				int b = s.charAt(1) - 'A';
-				map[a][b] = 1;
-				map[b][a] = 1;
-				if (in.hasNextLine()) {
-					s = in.nextLine();
+			int numRows = 0;
+			int numCols = 0;
+			
+			String line = in.nextLine();
+			// Get map info
+			while (line.charAt(0) == 'L' || line.charAt(0) == 'W') {
+				temp.add(line);
+				numRows++;
+				numCols = line.length();
+				line = in.nextLine();
+			}
+			
+//			System.out.printf("numRows: %d, numCols: %d\n", numRows, numCols);
+			
+			// Create map
+			map = new char[numRows][numCols];
+			visited = new int[numRows][numCols];
+			for (int i = 0; i < numRows; i++) {
+				map[i] = temp.get(i).toCharArray();
+			}
+			
+			// Get cell info
+			while (true) {
+				String[] array = line.split(" ");
+				int r = new Integer(array[0]) - 1;
+				int c = new Integer(array[1]) - 1;
+				System.out.println(count(r, c));
+				for (int i = 0; i < numRows; i++) {
+					Arrays.fill(visited[i], 0);;
+				}
+				if (in.hasNext()) {
+					line = in.nextLine();
 				} else {
 					break;
 				}
+				if (line.length() == 0) break;
 			}
-						
-			int answer = 0;
-			for (int i = 0; i < numNodes; i++) {
-				if (visited[i] == 0) {
-					solve(i);
-					answer++;
-				}
-			}
-			System.out.println(answer);
+			
 			if (numTests > 0) {
 				System.out.println();
 			}
 		}
 	}
-
-	public static void solve(int start) {
-		if (visited[start] == 1) {
-			return;
+	
+	public static int count(int r, int c) {
+		if (visited[r][c] == 1) {
+			return 0;
 		}
-		visited[start] = 1;
+		int answer = 1;
+		visited[r][c] = 1;
 		
-		for (int i = 0; i < map.length; i++) {
-			if (map[start][i] == 1 && visited[i] == 0) {
-				solve(i);
+		for (int i = 0; i < dx.length; i++) {
+			int newR = r + dx[i];
+			int newC = c + dy[i];
+			if (0 <= newR && newR < visited.length && 0 <= newC && newC < visited[0].length && map[newR][newC] == 'W') {
+				answer += count(newR, newC);
 			}
 		}
+		
+		return answer;
 	}
 }
