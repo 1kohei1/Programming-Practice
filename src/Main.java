a3f1692895f82532651a3610aac2f8742351d8be
Kohei Arai <tobecomebig@gmail.com>
2017-03-04 15:39:50 -0500

    uva 10004

--- src/Main.java
+++ src/Main.java
@@ -3,89 +3,70 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
-// uva 11060
+// uva 10004
 
 public class Main {
 
-	static HashMap<String, Drink> map;
+	static int[][] map;
+	static int[] color;
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 	
-		HashMap<String, Drink> map = new HashMap<String, Drink>();
-		PriorityQueue<Drink> pq = new PriorityQueue<Drink>();
-		int numTest = 1;
+		int n = in.nextInt();
+		while (n != 0) {
+			int l = in.nextInt();
+			
+			map = new int[n][n];
+			color = new int[n]; // Let use 1 and 2 to indicate bicolor
+			
+			for (int i = 0; i < l; i++) {
+				int a = in.nextInt();
+				int b = in.nextInt();
+				map[a][b] = 1;
+				map[b][a] = 1;
+			}
+			
+			if (isBicolord()) {
+				System.out.println("BICOLORABLE.");
+			} else {
+				System.out.println("NOT BICOLORABLE.");
+			}
+			
+			
+			n = in.nextInt();
+		}
+	}
+
+	public static boolean isBicolord() {
+		// Use color 1 at index 0
+		color[0] = 1;
 		
-		while (in.hasNext()) {
-			int n = in.nextInt();
-			for (int i = 0; i < n; i++) {
-				String s = in.next();
-				Drink d = new Drink(s, i);
-				map.put(s, d);
-				pq.add(d);
-			}
+		ArrayList<Integer> q = new ArrayList<Integer>();
+		q.add(0);
+		
+		while (q.size() > 0) {
+			int curr = q.remove(0);
+			int nextColor = color[curr] == 1 ? 2 : 1;
 			
-			int m = in.nextInt();
-			for (int i = 0; i < m; i++) {
-				String s1 = in.next();
-				String s2 = in.next();
-				
-				// Update connectingTo
-				Drink d;
-				d = map.get(s1);
-				d.connectingTo.add(s2);
-				
-				d = map.get(s2);
-				pq.remove(d);
-				d.numConnected++;
-				pq.add(d);
-			}
-			
-			// Output
-			System.out.printf("Case #%d: Dilbert should drink beverages in this order:", numTest);
-			while (pq.size() > 0) {
-				Drink d = pq.remove();
-				if (d.numConnected != 0) {
-					System.out.println("Something is wrong\n");
-					return;
+			for (int i = 0; i < map.length; i++) {
+				// If there is an edge and it is not colored yet.
+				if (map[curr][i] == 1 && color[i] == 0) {
+					// Check if neighbor node is already assigned a color. If they are assigned, make sure that color is different from nextColor
+					for (int j = 0; j < map.length; j++) {
+						if (map[i][j] == 1 && color[j] == nextColor) {
+							return false;
+						}
+					}
+					q.add(i);
+					color[i] = nextColor;
 				}
-				
-				for (int i = 0; i < d.connectingTo.size(); i++) {
-					Drink dd = map.get(d.connectingTo.get(i));
-					pq.remove(dd);
-					dd.numConnected--;
-					pq.add(dd);
-				}
-				System.out.printf(" %s", d.name);
 			}
-			System.out.println(".\n");
-			numTest++;
 		}
-	}
-}
-
-class Drink implements Comparable<Drink> {
-
-	int order;
-	String name;
-	ArrayList<String> connectingTo;
-	int numConnected;
-	
-	public Drink(String name, int order) {
-		this.order = order;
-		this.name = name;
-		this.connectingTo = new ArrayList<String>();
-		this.numConnected = 0;
-	}
-	
-	@Override
-	public int compareTo(Drink o) {
-		if (this.numConnected - o.numConnected != 0) {
-			return this.numConnected - o.numConnected;
-		} else {
-			return this.order - o.order;
-		}
+		return true;
+		
 	}
 	
 }
