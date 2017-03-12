7a8bda9aa725f3af58b6d47a5c5c266e308e8cc6
Kohei Arai <tobecomebig@gmail.com>
2017-03-12 00:40:45 -0500

    UVa 10199

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,78 @@
-import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.Scanner;
 
-// UVa 315
+// UVa 10199
 
 public class Main {
 
-	static ArrayList<Integer>[] map;
+	static HashMap<String, Integer> cities;
+	static String[] cityNames;
+	static ArrayList[] map;
 	static int dfs_counter;
-	static int[] visited;
 	static int[] dfs_num;
 	static int[] dfs_low;
 	static int[] isArticulation;
+	static ArrayList<String> answerNames;
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-	
+		
 		int n = in.nextInt();
-		in.nextLine();
+		int numTests = 1;
+		
+		cities = new HashMap<String, Integer>();
+		answerNames = new ArrayList<String>();
 		
 		while (n > 0) {
-			
-			// Initialize variable
-			visited = new int[n];
-			dfs_counter = 0;
-			isArticulation = new int[n];
+			if (numTests != 1) {
+				System.out.println();
+			}
+			// Initialize variables
+			cities.clear();
+			map = new ArrayList[n];
+			dfs_counter = 1;
 			dfs_num = new int[n];
 			dfs_low = new int[n];
-			map = new ArrayList[n];
+			cityNames = new String[n];
+			answerNames.clear();
+			isArticulation = new int[n];
+			
+			// Get city name
 			for (int i = 0; i < n; i++) {
+				String name = in.next();
+				cities.put(name, i);
+				cityNames[i] = name;
 				map[i] = new ArrayList<Integer>();
+ 			}
+			
+			int r = in.nextInt();
+			for (int i = 0; i < r; i++) {
+				String a = in.next();
+				String b = in.next();
+				
+				int ai = cities.get(a);
+				int bi = cities.get(b);
+				
+				map[ai].add(bi);
+				map[bi].add(ai);
 			}
 			
-			String s = in.nextLine();
-			while (s.length() != 1) {
-				String[] arr = s.split(" ");
-				
-				int start = new Integer(arr[0]) - 1;
-				for (int i = 1; i < arr.length; i++) {
-					int dst = new Integer(arr[i]) - 1;
-					map[start].add(dst);
-					map[dst].add(start);
-				}
-				
-				s = in.nextLine();
-			}
-			
-			solve(-1, 0);
-			
-			int numArticulation = 0;
 			for (int i = 0; i < n; i++) {
-				numArticulation += isArticulation[i];
+				if (dfs_num[i] == 0) {
+					solve(-1, i);
+				}
 			}
 			
-			System.out.println(numArticulation);
+			answerNames.sort(null);
+			System.out.printf("City map #%d: %d camera(s) found\n", numTests, answerNames.size());
+			for (int i = 0; i < answerNames.size(); i++) {
+				System.out.println(answerNames.get(i));
+			}
 			
+			// Get next test set
 			n = in.nextInt();
-			in.nextLine();
+			numTests++;
 		}
 	}
 	
@@ -68,31 +81,31 @@ public class Main {
 		dfs_low[curr] = dfs_counter;
 		dfs_counter++;
 		
-		visited[curr] = 1;
-		ArrayList<Integer> edges = map[curr];
-		
 		int numChildren = 0;
+		ArrayList<Integer> edge = map[curr];
 		
-		for (int i = 0; i < edges.size(); i++) {
-			int next = edges.get(i);
+		for (int i = 0; i < edge.size(); i++) {
+			int next = edge.get(i);
 			
-			if (next == parent) continue;
-			
-			if (visited[next] == 1) {
-				dfs_low[curr] = Math.min(dfs_low[curr], dfs_num[next]);
-			} else {
+			// Unvisited node
+			if (dfs_num[next] == 0) {
 				numChildren++;
 				solve(curr, next);
 				
 				dfs_low[curr] = Math.min(dfs_low[curr], dfs_low[next]);
 				
-				if (parent == -1 && numChildren > 1) {
-					isArticulation[curr] = 1;
-				}
-				if (parent != -1 && dfs_num[curr] <= dfs_low[next]) {
-					isArticulation[curr] = 1;
+				if (parent != -1 && dfs_num[curr] <= dfs_low[next] && !answerNames.contains(cityNames[curr])) {
+					answerNames.add(cityNames[curr]);
 				}
 			}
+			// It's visited node, but not equal to the parent
+			else if (next != parent) {
+				dfs_low[curr] = Math.min(dfs_low[curr], dfs_num[next]);
+			}
+		}
+		
+		if (parent == -1 && numChildren > 1 && !answerNames.contains(cityNames[curr])) {
+			answerNames.add(cityNames[curr]);
 		}
 	}
 }
