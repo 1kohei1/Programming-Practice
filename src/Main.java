7e93524610f388fed390af0df21c58f0e591a02e
Kohei Arai <tobecomebig@gmail.com>
2017-03-15 09:29:01 -0400

    Temp UVa 11504

--- src/Main.java
+++ src/Main.java
@@ -1,119 +1,86 @@
 import java.util.*;
 
-// UVa 247
+// UVa 11504
 
 public class Main {
 
-	static HashMap<String, Integer> nameMap;
-	static ArrayList[] map;
-	static String[] names;
-	static int[] dfs_num;
-	static int[] dfs_low;
-	static Stack<Integer> stack;
-	static int[] inStack;
-	static int dfs_counter;
-	
-	static int numTests;
+	static int answer;
+	static Node[] map;
+	static int[] visited;
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		numTests = 1;
+		int numTests = in.nextInt();
 		
-		int n = in.nextInt();
-		int m = in.nextInt();
-		nameMap = new HashMap<String, Integer>();
-		stack = new Stack<Integer>();
-		
-		while (0 < n || 0 < m) {
+		while (numTests-- > 0) {
+			int n = in.nextInt();
+			int m = in.nextInt();
+			
 			// Initialize variables
-			dfs_counter = 1;
-			dfs_num = new int[n];
-			dfs_low = new int[n];
-			names = new String[n];
-			nameMap.clear();
-			inStack = new int[n];
-			map = new ArrayList[n];
+			map = new Node[n];
+			answer = 0;
+			visited = new int[n];
 			for (int i = 0; i < n; i++) {
-				map[i] = new ArrayList<Integer>();
+				map[i] = new Node(i);
 			}
 			
-			// Get map
-			int index = 0;
-			for (int i = 0; i < m; i++) {
-				String a = in.next();
-				String b = in.next();
-
-				int ai = -1;
-				int bi = -1;
-				
-				if (!nameMap.containsKey(a)) {
-					nameMap.put(a, index);
-					names[index] = a;
-					ai = index;
-					index++;
-				} else {
-					ai = nameMap.get(a);
-				}
-				if (!nameMap.containsKey(b)) {
-					nameMap.put(b, index);
-					names[index] = b;
-					bi = index;
-					index++;
-				} else {
-					bi = nameMap.get(b);
-				}
-				
-				map[ai].add(bi);
+			while (m-- > 0) {
+				int a = in.nextInt() - 1;
+				int b = in.nextInt() - 1;
+				map[a].edge.add(b);
+				map[b].numIncoming++;
 			}
 			
-			System.out.printf("Calling circles for data set %d:\n", numTests);
-			for (int i = 0; i < n; i++) {
-				if (dfs_num[i] == 0) {
-					solve(i);
-				}
-			}
+			topoSort();
+			
+			System.out.println(answer);
+		}
+	}
 
-			n = in.nextInt();
-			m = in.nextInt();
-			if (n != 0 && m != 0) {
-				System.out.println();
+	public static void topoSort() {
+		ArrayList<Node> list = new ArrayList<Node>();
+		for (int i = 0; i < map.length; i++) {
+			list.add(map[i]);
+		}
+		list.sort(null);
+		
+		for (int i = 0; i < list.size(); i++) {
+			Node n = list.get(i);
+			
+			if (visited[n.v] == 0) {
+				answer++;
+				visit(n);
 			}
-			numTests++;
 		}
 	}
 	
-	public static void solve(int curr) {
-		dfs_num[curr] = dfs_counter;
-		dfs_low[curr] = dfs_counter;
-		dfs_counter++;
-		
-		inStack[curr] = 1;
-		stack.push(curr);
-		ArrayList<Integer> edge = map[curr];
-		
-		for (int i = 0; i < edge.size(); i++) {
-			int next = edge.get(i);
-			
-			// If not visited yet, visit.
-			if (dfs_num[next] == 0) {
-				solve(next);
+	public static void visit(Node n) {
+		visited[n.v] = 1;
+
+		for (int i = 0; i < n.edge.size(); i++) {
+			if (visited[n.edge.get(i)] == 0) {
+				visit(map[n.edge.get(i)]);
 			}
-			// Isn't this always true?
-			if (inStack[next] == 1) {
-				dfs_low[curr] = Math.min(dfs_low[curr], dfs_low[next]);
-			}
-		}
-		
-		if (dfs_low[curr] == dfs_num[curr]) {
-			int pop = stack.pop();
-			while (pop != curr) {
-				inStack[pop] = 0;
-				System.out.printf("%s, ", names[pop]);
-				pop = stack.pop();
-			}
-			inStack[pop] = 0;
-			System.out.println(names[pop]);
 		}
 	}
+}
+
+class Node implements Comparable<Node> {
+	int v;
+	int numIncoming;
+	ArrayList<Integer> edge;
+	
+	public Node(int v) {
+		this.v = v;
+		this.numIncoming = 0;
+		edge = new ArrayList<Integer>();
+	}
+	
+	@Override
+	public int compareTo(Node o) {
+		return this.numIncoming - o.numIncoming;
+	}
+	
+	
 }
