e49d7dea38f099e8231cef14604cfab1139ba382
Kohei Arai <tobecomebig@gmail.com>
2017-03-15 08:13:30 -0400

    Correct implementation of SCC

--- src/Playground.java
+++ src/Playground.java
@@ -11,6 +11,7 @@ public class Playground {
 	static int dfs_counter;
 	static ArrayList[] map;
 	static Stack<Integer> stack;
+	static int[] inStack;
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -24,7 +25,7 @@ public class Playground {
 			dfs_low = new int[n];
 			dfs_counter = 1;
 			map = new ArrayList[n];
-			
+			inStack = new int[n];
 			for (int i = 0; i < n; i++) {
 				map[i] = new ArrayList<Integer>();
 			}
@@ -53,6 +54,7 @@ public class Playground {
 		dfs_counter++;
 		
 		stack.add(curr);
+		inStack[curr] = 1;
 		ArrayList<Integer> edge = map[curr];
 		
 		for (int i = 0; i < edge.size(); i++) {
@@ -62,18 +64,21 @@ public class Playground {
 			if (dfs_num[next] == 0) {
 				solve(next);
 			}
-			
-			dfs_low[curr] = Math.min(dfs_low[curr], dfs_low[next]);
-			
-			if (dfs_num[curr] == dfs_low[curr]) {
-				System.out.printf("SCC (");
-				int pop = stack.pop();
-				while (pop != curr) {
-					System.out.printf("%d ", pop);
-					pop = stack.pop();
-				}
-				System.out.printf("%d)\n", pop);
+			if (inStack[next] == 1) {
+				dfs_low[curr] = Math.min(dfs_low[curr], dfs_low[next]);
 			}
+		}
+		
+		if (dfs_num[curr] == dfs_low[curr]) {
+			System.out.printf("SCC (");
+			int pop = stack.pop();
+			while (pop != curr) {
+				inStack[pop] = 0;
+				System.out.printf("%d ", pop);
+				pop = stack.pop();
+			}
+			inStack[pop] = 0;
+			System.out.printf("%d)\n", pop);
 		}
 	}
 }
@@ -90,5 +95,82 @@ public class Playground {
 5 7
 7 6
 6 4
-0
+=>
+SCC (6 7 5 4)
+SCC (2 3 1)
+SCC (0)
+
+5 6
+0 1
+1 4
+1 2
+2 0
+2 3
+3 2
+=> 
+SCC (4)
+SCC (0 1 3 2)
+4 5
+0 1
+1 2
+2 0
+0 3
+3 0
+=>
+SCC (3 2 1 0)
+3 2
+1 0
+2 0
+=>
+SCC (0)
+SCC (1)
+SCC (2)
+5 5
+0 1
+1 2
+2 0
+0 4
+3 0
+0 0
+=>
+SCC (4)
+SCC (2 1 0)
+SCC (3)
+
+4 3
+0 1
+1 2
+2 3
+=>
+SCC (3)
+SCC (2)
+SCC (1)
+SCC (0)
+
+6 7
+0 1
+1 2
+2 3
+3 4
+4 2
+1 5
+5 2
+=>
+SCC (4 3 2)
+SCC (5)
+SCC (1)
+SCC (0)
+
+6 7
+0 1
+1 2
+2 3
+3 4
+5 0
+4 2
+1 5
+5 2
+=>
+SCC (4 3 2)
+SCC (5 1 0)
  */
