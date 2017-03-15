e8efe90097a926b812ae596ca1f12a24cf365daa
Kohei Arai <tobecomebig@gmail.com>
2017-03-15 09:43:17 -0400

    UVa 11838

--- src/Main.java
+++ src/Main.java
@@ -1,86 +1,88 @@
 import java.util.*;
 
-// UVa 11504
+// UVa 11838
 
 public class Main {
 
-	static int answer;
-	static Node[] map;
-	static int[] visited;
+	static int[] dfs_num;
+	static int[] dfs_low;
+	static int dfs_counter;
+	static ArrayList[] map;
+	static Stack<Integer> stack;
+	static int[] inStack;
+	static int numSCC;
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		int numTests = in.nextInt();
+		int n = in.nextInt();
+		int m = in.nextInt();
+		stack = new Stack<Integer>();
 		
-		while (numTests-- > 0) {
-			int n = in.nextInt();
-			int m = in.nextInt();
-			
-			// Initialize variables
-			map = new Node[n];
-			answer = 0;
-			visited = new int[n];
+		while (0 < n && 0 < m) {
+			// Initialize
+			dfs_num = new int[n];
+			dfs_low = new int[n];
+			dfs_counter = 1;
+			inStack = new int[n];
+			numSCC = 0;
+			map = new ArrayList[n];
 			for (int i = 0; i < n; i++) {
-				map[i] = new Node(i);
+				map[i] = new ArrayList<Integer>();
 			}
 			
 			while (m-- > 0) {
 				int a = in.nextInt() - 1;
 				int b = in.nextInt() - 1;
-				map[a].edge.add(b);
-				map[b].numIncoming++;
+				int c = in.nextInt();
+				
+				map[a].add(b);
+				if (c == 2) {
+					map[b].add(a);
+				}
 			}
 			
-			topoSort();
+			for (int i = 0; i < n; i++) {
+				if (dfs_num[i] == 0) {
+					countCSS(i);
+				}
+			}
 			
-			System.out.println(answer);
+			System.out.println(numSCC == 1 ? 1 : 0);
+			
+			n = in.nextInt();
+			m = in.nextInt();
 		}
 	}
-
-	public static void topoSort() {
-		ArrayList<Node> list = new ArrayList<Node>();
-		for (int i = 0; i < map.length; i++) {
-			list.add(map[i]);
-		}
-		list.sort(null);
+	
+	public static void countCSS(int curr) {
+		dfs_num[curr] = dfs_counter;
+		dfs_low[curr] = dfs_counter;
+		dfs_counter++;
 		
-		for (int i = 0; i < list.size(); i++) {
-			Node n = list.get(i);
+		stack.push(curr);
+		inStack[curr] = 1;
+		ArrayList<Integer> edge = map[curr];
+		
+		for (int i = 0; i < edge.size(); i++) {
+			int next = edge.get(i);
 			
-			if (visited[n.v] == 0) {
-				answer++;
-				visit(n);
+			if (dfs_num[next] == 0) {
+				countCSS(next);
+			}
+			if (inStack[next] == 1) {
+				dfs_low[curr] = Math.min(dfs_low[curr], dfs_low[next]);
 			}
 		}
-	}
-	
-	public static void visit(Node n) {
-		visited[n.v] = 1;
-
-		for (int i = 0; i < n.edge.size(); i++) {
-			if (visited[n.edge.get(i)] == 0) {
-				visit(map[n.edge.get(i)]);
+		
+		if (dfs_num[curr] == dfs_low[curr]) {
+			numSCC++;
+			int pop = stack.pop();
+			while (pop != curr) {
+				inStack[pop] = 0;
+				pop = stack.pop();
 			}
+			inStack[pop] = 0;
 		}
 	}
-}
-
-class Node implements Comparable<Node> {
-	int v;
-	int numIncoming;
-	ArrayList<Integer> edge;
-	
-	public Node(int v) {
-		this.v = v;
-		this.numIncoming = 0;
-		edge = new ArrayList<Integer>();
-	}
-	
-	@Override
-	public int compareTo(Node o) {
-		return this.numIncoming - o.numIncoming;
-	}
-	
-	
 }
