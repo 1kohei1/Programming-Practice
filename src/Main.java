6590e4bd4b6111f9dd3365f9f125addcb9f50f2e
Kohei Arai <tobecomebig@gmail.com>
2017-03-15 08:14:09 -0400

    UVa 247

--- src/Main.java
+++ src/Main.java
@@ -1,114 +1,119 @@
 import java.util.*;
 
-// UVa 796
+// UVa 247
 
 public class Main {
 
+	static HashMap<String, Integer> nameMap;
 	static ArrayList[] map;
-	static int dfs_counter;
+	static String[] names;
 	static int[] dfs_num;
 	static int[] dfs_low;
-	static ArrayList<Bridge> bridges;
+	static Stack<Integer> stack;
+	static int[] inStack;
+	static int dfs_counter;
+	
+	static int numTests;
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		bridges = new ArrayList<Bridge>();
+		numTests = 1;
 		
-		while (in.hasNext()) {
-			int n = in.nextInt();
+		int n = in.nextInt();
+		int m = in.nextInt();
+		nameMap = new HashMap<String, Integer>();
+		stack = new Stack<Integer>();
+		
+		while (0 < n || 0 < m) {
 			// Initialize variables
 			dfs_counter = 1;
-			bridges.clear();
 			dfs_num = new int[n];
 			dfs_low = new int[n];
+			names = new String[n];
+			nameMap.clear();
+			inStack = new int[n];
 			map = new ArrayList[n];
 			for (int i = 0; i < n; i++) {
 				map[i] = new ArrayList<Integer>();
 			}
 			
-			for (int i = 0; i < n; i++) {
-				int start = in.nextInt();
-				String s = in.next();
-				int numEdges = new Integer(s.substring(1, s.length() - 1));
-				for (int j = 0; j < numEdges; j++) {
-					int end = in.nextInt();
-					map[start].add(end);
+			// Get map
+			int index = 0;
+			for (int i = 0; i < m; i++) {
+				String a = in.next();
+				String b = in.next();
+
+				int ai = -1;
+				int bi = -1;
+				
+				if (!nameMap.containsKey(a)) {
+					nameMap.put(a, index);
+					names[index] = a;
+					ai = index;
+					index++;
+				} else {
+					ai = nameMap.get(a);
 				}
+				if (!nameMap.containsKey(b)) {
+					nameMap.put(b, index);
+					names[index] = b;
+					bi = index;
+					index++;
+				} else {
+					bi = nameMap.get(b);
+				}
+				
+				map[ai].add(bi);
 			}
 			
-			// Find articulation bridges
+			System.out.printf("Calling circles for data set %d:\n", numTests);
 			for (int i = 0; i < n; i++) {
 				if (dfs_num[i] == 0) {
-					articulation(-1, i);
+					solve(i);
 				}
 			}
-			
-			bridges.sort(null);
-			
-			// Print
-			System.out.printf("%d critical links\n", bridges.size());
-			for (int i = 0; i < bridges.size(); i++) {
-				Bridge b = bridges.get(i);
-				System.out.printf("%d - %d\n", b.start, b.end);
+
+			n = in.nextInt();
+			m = in.nextInt();
+			if (n != 0 && m != 0) {
+				System.out.println();
 			}
-			
-			System.out.println();
+			numTests++;
 		}
-		
 	}
 	
-	public static void articulation(int parent, int curr) {
+	public static void solve(int curr) {
 		dfs_num[curr] = dfs_counter;
 		dfs_low[curr] = dfs_counter;
 		dfs_counter++;
 		
+		inStack[curr] = 1;
+		stack.push(curr);
 		ArrayList<Integer> edge = map[curr];
 		
 		for (int i = 0; i < edge.size(); i++) {
 			int next = edge.get(i);
 			
-			// Not visited yet
+			// If not visited yet, visit.
 			if (dfs_num[next] == 0) {
-				articulation(curr, next);
-				
+				solve(next);
+			}
+			// Isn't this always true?
+			if (inStack[next] == 1) {
 				dfs_low[curr] = Math.min(dfs_low[curr], dfs_low[next]);
-			
-				if (dfs_num[curr] < dfs_low[next]) {
-					bridges.add(new Bridge(curr, next));
-				}
-			} 
-			// Visited node and not parent
-			else if (next != parent) {
-				dfs_low[curr] = Math.min(dfs_low[curr], dfs_num[next]);
 			}
 		}
 		
-	}
-}
-
-class Bridge implements Comparable<Bridge> {
-	int start;
-	int end;
-	
-	public Bridge(int start, int end) {
-		if (start < end) {
-			this.start = start;
-			this.end = end;
-		} else {
-			this.start = end;
-			this.end = start;
+		if (dfs_low[curr] == dfs_num[curr]) {
+			int pop = stack.pop();
+			while (pop != curr) {
+				inStack[pop] = 0;
+				System.out.printf("%s, ", names[pop]);
+				pop = stack.pop();
+			}
+			inStack[pop] = 0;
+			System.out.println(names[pop]);
 		}
 	}
-	
-	@Override
-	public int compareTo(Bridge o) {
-		if (this.start == o.start) {
-			return this.end - o.end;
-		} else {
-			return this.start - o.start;
-		}
-	}
-	
-	
 }
