e00fe5db17352d4513fa17a3854963831812dcf9
Kohei Arai <tobecomebig@gmail.com>
2017-03-23 07:38:57 -0400

    ABC 50-C

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,52 @@
 import java.util.*;
 
-// UVa 11504
+// ABC 50-C
+// http://abc050.contest.atcoder.jp/tasks/arc066_a
 
 public class Main {
-
-	static int n;
-	static int[] visited;
-	static int answer;
-	static int index;
-	static int[] ts;
-	static ArrayList[] map;
 	
+	static int MOD = 1000000007;
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-
-		int numTests = in.nextInt();
 		
-		while (numTests-- > 0) {
-			n = in.nextInt();
-			int m = in.nextInt();
-			
-			map = new ArrayList[n];
-			ts = new int[n];
-			visited = new int[n];
-			for (int i = 0; i < n; i++) {
-				map[i] = new ArrayList<Integer>();
+		int n = in.nextInt();
+		int[] freq = new int[n];
+		for (int i = 0; i < n; i++) {
+			freq[in.nextInt()]++;
+		}
+		
+		boolean isValid = true;
+		
+		if (n % 2 == 1) {
+			if (freq[0] != 1) {
+				isValid = false;
 			}
-			
-			while (m-- > 0) {
-				int a = in.nextInt() - 1;
-				int b = in.nextInt() - 1;
-				
-				if (!map[a].contains(b)) {
-					map[a].add(b);
+			for (int i = 2; i < n; i += 2) {
+				if (freq[i] != 2) {
+					isValid = false;
+					break;
 				}
 			}
-			
-			// Topological sort
-			index = n - 1;
-			for (int i = 0; i < n; i++) {
-				if (visited[i] == 0) {
-					toposort(i);
+		} else {
+			for (int i = 1; i < n; i += 2) {
+				if (freq[i] != 2) {
+					isValid = false;
+					break;
 				}
 			}
-			
-			// Start from node in ts
+		}
+		
+		long answer = 1;
+		if (isValid) {
+			for (int i = 0; i < n / 2; i++) {
+				answer *= 2;
+				answer %= MOD;
+			}
+		} else {
 			answer = 0;
-			Arrays.fill(visited, 0);
-			for (int i = 0; i < n; i++) {
-				if (visited[ts[i]] == 0) {
-					answer++;
-					dfs(ts[i]);
-				}
-			}
-			System.out.println(answer);
 		}
-	}
-	
-	public static void dfs(int curr) {
-		visited[curr] = 1;
-		for (int i = 0; i < map[curr].size(); i++) {
-			if (visited[(int) map[curr].get(i)] == 0) {
-				dfs((int) map[curr].get(i));
-			}
-		}
-	}
-	
-	public static void toposort(int curr) {
-		visited[curr] = 1;
-		for (int i = 0; i < map[curr].size(); i++) {
-			if (visited[(int) map[curr].get(i)] == 0) {
-				toposort((int) map[curr].get(i));
-			}
-		}
-		ts[index] = curr;
-		index--;
+		System.out.println(answer);
+		
 	}
 }
