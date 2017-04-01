c32c4741598561ccfd48061fc0b93401c1d01310
Kohei Arai <tobecomebig@gmail.com>
2017-04-01 07:43:13 -0400

    UVa 11450 bottom up dp

--- src/Main.java
+++ src/Main.java
@@ -5,54 +5,56 @@ import java.util.*;
 
 public class Main {
 	
-	static int c;
-	static int m;
-	static long[][] memo;
-
 	public static void main (String[] args) throws java.lang.Exception {
 	    Scanner in = new Scanner(System.in);
 	    
 	    int numTest = in.nextInt();
-	    ArrayList[] garment = new ArrayList[2];
 	    
 	    while (numTest-- > 0) {
-	    	m = in.nextInt();
-	    	c = in.nextInt();
+	    	int m = in.nextInt();
+	    	int c = in.nextInt();
 	    	
-		    garment = new ArrayList[c];
-		    memo = new long[c][m + 1];
-		    
-		    for (int i = 0; i < c; i++) {
-		    	garment[i] = new ArrayList<Integer>();
-		    	Arrays.fill(memo[i], -1);
-		    	int k = in.nextInt();
-		    	for (int j = 0; j < k; j++) {
-		    		garment[i].add(in.nextInt());
-		    	}
-		    }
-		    
-		    long answer = solve(garment, 0, 0);
-		    System.out.println(answer == -1 ? "no solution" : answer);
+	    	ArrayList[] garment = new ArrayList[c];
+	    	int[][] dp = new int[c][m + 1]; 
+	    	// dp[c][m] means in the range of garment0 to garmentc, it's possible to spend the m amount of money
+	    	
+	    	for (int i = 0; i < c; i++) {
+	    		int k = in.nextInt();
+	    		garment[i] = new ArrayList<Integer>();
+	    		
+	    		for (int j = 0; j < k; j++) {
+	    			garment[i].add(in.nextInt());
+	    		}
+	    	}
+	    	
+	    	// First item
+	    	for (int i = 0; i < garment[0].size(); i++) {
+	    		int p = (int) garment[0].get(i);
+	    		if (0 < p && p < m) {
+	    			dp[0][p] = 1;
+	    		}
+	    	}
+	    	
+	    	// from second item
+	    	for (int i = 1; i < c; i++) {
+	    		for (int j = 0; j < garment[i].size(); j++) {
+	    			int p = (int) garment[i].get(j);
+	    			for (int k = 0; k < m + 1; k++) {
+	    				if (dp[i - 1][k] == 1 && k + p <= m) {
+	    					dp[i][k + p] = 1;
+	    				}
+	    			}
+	    		}
+	    	}
+	    	
+	    	int answer = -1;
+	    	for (int i = m; i > 0; i--) {
+	    		if (dp[c - 1][i] == 1) {
+	    			answer = i;
+	    			break;
+	    		}
+	    	}
+	    	System.out.println(answer == -1 ? "no solution" : answer);
 	    }
-	    
-	}
-	
-	public static long solve(ArrayList[] garment, int index, int usedMoney) {
-		if (usedMoney > m) {
-			return -1;
-		}
-		if (index == c) {
-			return usedMoney;
-		}
-		if (memo[index][usedMoney] >= 0) {
-			return memo[index][usedMoney];
-		}
-		long answer = -1;
-		// Think about buying a garment at index
-		for (int i = 0; i < garment[index].size(); i++) {
-			answer = Math.max(answer, solve(garment, index + 1, usedMoney + (int) garment[index].get(i)));
-		}
-		memo[index][usedMoney] = answer;
-		return answer;
 	}
 }
