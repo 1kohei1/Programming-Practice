abb8ea1d67b6852f499c9f5d69fd08286c9a7304
Kohei Arai <tobecomebig@gmail.com>
2017-04-01 17:39:55 -0400

    ABC 7-C

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,66 @@
 import java.util.*;
  
-// ABC 10-C
-// http://abc010.contest.atcoder.jp/tasks/abc010_3
+// ABC 7-C
+// https://abc007.contest.atcoder.jp/tasks/abc007_3
  
 public class Main {
  
+	static int[] dx = new int[]{-1, 0, 1, 0};
+	static int[] dy = new int[]{0, -1, 0, 1};
+	
 	public static void main (String[] args) throws java.lang.Exception {
 	    Scanner in = new Scanner(System.in);
+	   
+	    int r = in.nextInt();
+	    int c = in.nextInt();
 	    
-	    int x1 = in.nextInt();
-	    int y1 = in.nextInt();
-	    int x2 = in.nextInt();
-	    int y2 = in.nextInt();
+	    int sx = in.nextInt() - 1;
+	    int sy = in.nextInt() - 1;
 	    
-	    int t = in.nextInt();
-	    int v = in.nextInt();
-	    int maxd = t * v;
+	    int gx = in.nextInt() - 1;
+	    int gy = in.nextInt() - 1;
 	    
-	    int n = in.nextInt();
-	    int count = 0;
-	    for (int i = 0; i < n; i++) {
-	    	int x = in.nextInt();
-	    	int y = in.nextInt();
+	    char[][] map = new char[r][c];
+	    for (int i = 0; i < r; i++) {
+	    	map[i] = in.next().toCharArray();
+	    }
+	    
+	    int answer = 0;
+	    ArrayList<Integer> q = new ArrayList<Integer>();
+	    q.add(sx);
+	    q.add(sy);
+	    q.add(0);
+	    
+	    while (q.size() > 0) {
+	    	int cx = q.remove(0);
+	    	int cy = q.remove(0);
+	    	int step = q.remove(0);
+	    
+	    	if (cx == gx && cy == gy) {
+	    		answer = step;
+	    		break;
+	    	}
 	    	
-	    	double d = dist(x1, y1, x, y) + dist(x, y, x2, y2);
-	    	if (d <= maxd) {
-	    		count++;
+	    	// This node is already visited.
+	    	if (map[cx][cy] == '#') {
+	    		continue;
+	    	}
+	    	
+	    	// Make it visited
+	    	map[cx][cy] = '#';
+	    	
+	    	for (int i = 0; i < 4; i++) {
+	    		int nx = cx + dx[i];
+	    		int ny = cy + dy[i];
+	    		
+	    		if (0 <= nx && nx < r && 0 <= ny && ny < c && map[nx][ny] == '.') {
+	    			q.add(nx);
+	    			q.add(ny);
+	    			q.add(step + 1);
+	    		}
 	    	}
 	    }
 	    
-	    System.out.println(count > 0 ? "YES" : "NO");
-	}
-	
-	public static double dist(int x1, int y1, int x2, int y2) {
-		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+	    System.out.println(answer);
 	}
 }
