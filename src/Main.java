3629d76d06b6a667d54bffa3cba1edb32fcfef57
Kohei Arai <tobecomebig@gmail.com>
2017-04-01 16:06:15 -0400

    ABC 12-C

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,21 @@
 import java.util.*;
 
-// ABC 16-C
-// http://abc016.contest.atcoder.jp/tasks/abc016_3
+// ABC 12-C
+// https://abc012.contest.atcoder.jp/submissions/me#
 
 public class Main {
 
-	static int n;
-	static int[][] map;
-	
 	public static void main (String[] args) throws java.lang.Exception {
 	    Scanner in = new Scanner(System.in);
 	    
-	    n = in.nextInt();
-	    int m = in.nextInt();
+	    int total = 2025;
+	    int n = in.nextInt();
+	    int diff = total - n;
 	    
-	    map = new int[n][n];
-	    for (int i = 0; i < m; i++) {
-	    	int a = in.nextInt() - 1;
-	    	int b = in.nextInt() - 1;
-	    	map[a][b] = 1;
-	    	map[b][a] = 1;
+	    for (int i = 1; i <= 9; i++) {
+	    	if (diff % i == 0 && diff / i <= 9) {
+	    		System.out.printf("%d x %d\n", i, diff / i);
+	    	}
 	    }
-	    
-	    for (int i = 0; i < n; i++) {
-	    	System.out.println(solve(i));
-	    }
-	}
-	
-	public static int solve(int start) {
-		int count = 0;
-		ArrayList<Integer> counted = new ArrayList<Integer>();
-		for (int i = 0; i < n; i++) {
-			if (map[start][i] == 1) {
-				for (int j = 0; j < n; j++) {
-					if (map[i][j] == 1 && start != j && map[start][j] == 0 && !counted.contains(j)) {
-						count++;
-						counted.add(j);
-					}
-				}
-			}
-		}
-		return count;
 	}
 }
