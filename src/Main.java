9670b3140f344b1568a3d34049ab163b8ea73134
Kohei Arai <tobecomebig@gmail.com>
2017-04-01 08:20:10 -0400

    ABC 43-C

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,32 @@
 import java.util.*;
 
-// UVa 11450
-// https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=2445
+// ABC 43-C
+// http://abc043.contest.atcoder.jp/tasks/arc059_a
 
 public class Main {
 	
 	public static void main (String[] args) throws java.lang.Exception {
 	    Scanner in = new Scanner(System.in);
 	    
-	    int numTest = in.nextInt();
+	    int n = in.nextInt();
+	    int[] nums = new int[n];
+	    int sum = 0;
 	    
-	    while (numTest-- > 0) {
-	    	int m = in.nextInt();
-	    	int c = in.nextInt();
-	    	
-	    	ArrayList[] garment = new ArrayList[c];
-	    	int[][] dp = new int[c][m + 1]; 
-	    	// dp[c][m] means in the range of garment0 to garmentc, it's possible to spend the m amount of money
-	    	
-	    	for (int i = 0; i < c; i++) {
-	    		int k = in.nextInt();
-	    		garment[i] = new ArrayList<Integer>();
-	    		
-	    		for (int j = 0; j < k; j++) {
-	    			garment[i].add(in.nextInt());
-	    		}
-	    	}
-	    	
-	    	// First item
-	    	for (int i = 0; i < garment[0].size(); i++) {
-	    		int p = (int) garment[0].get(i);
-	    		if (0 < p && p < m) {
-	    			dp[0][p] = 1;
-	    		}
-	    	}
-	    	
-	    	// from second item
-	    	for (int i = 1; i < c; i++) {
-	    		for (int j = 0; j < garment[i].size(); j++) {
-	    			int p = (int) garment[i].get(j);
-	    			for (int k = 0; k < m + 1; k++) {
-	    				if (dp[i - 1][k] == 1 && k + p <= m) {
-	    					dp[i][k + p] = 1;
-	    				}
-	    			}
-	    		}
-	    	}
-	    	
-	    	int answer = -1;
-	    	for (int i = m; i > 0; i--) {
-	    		if (dp[c - 1][i] == 1) {
-	    			answer = i;
-	    			break;
-	    		}
-	    	}
-	    	System.out.println(answer == -1 ? "no solution" : answer);
+	    for (int i = 0; i < n; i++) {
+	    	nums[i] = in.nextInt();
+	    	sum += nums[i];
 	    }
+	    
+	    int avg = sum / n; // Probably need to investigate avg + 1 too
+	    
+	    System.out.println(Math.min(cost(nums, avg), cost(nums, avg + 1)));
+	}
+	
+	public static int cost(int[] nums, int avg) {
+		int cost = 0;
+		for (int i = 0; i < nums.length; i++) {
+			cost += (nums[i] - avg) * (nums[i] - avg);
+		}
+		return cost;
 	}
 }
