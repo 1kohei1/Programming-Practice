6e88ea7db87e7e031945a2b9c975984e4a58ae16
Kohei Arai <tobecomebig@gmail.com>
2017-04-01 11:49:43 -0400

    ABC 33-C

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,25 @@
 import java.util.*;
 
-// ABC 37-C
-// http://abc038.contest.atcoder.jp/tasks/abc038_c
+// ABC 33-C
+// http://abc033.contest.atcoder.jp/tasks/abc033_c
 
 public class Main {
+
+	static long MOD = 1000000007;
 	
 	public static void main (String[] args) throws java.lang.Exception {
 	    Scanner in = new Scanner(System.in);
 	    
-	    int n = in.nextInt();
-	    int k = in.nextInt();
+	    String[] s = in.next().split("\\+");
 	    
-	    int[] nums = new int[n];
 	    long answer = 0;
-	    long partialSum = 0;
-	    
-	    for (int i = 0; i < n; i++) {
-	    	nums[i] = in.nextInt();
-	    	if (i < k) {
-	    		partialSum += nums[i];
+	    for (int i = 0; i < s.length; i++) {
+	    	if (s[i].length() == 1 && s[i].charAt(0) != '0') {
+	    		answer++;
+	    	} else if (s[i].contains("*") && s[i].indexOf('0') == -1) {
+	    		answer++;
 	    	}
-	    }
-	    
-	    answer = partialSum;
-	    
-	    for (int i = 1; i < n - k + 1; i++) {
-	    	partialSum -= nums[i - 1];
-	    	partialSum += nums[i + k - 1];
-	    	answer += partialSum;
+	    	answer %= MOD;
 	    }
 	    System.out.println(answer);
 	}
