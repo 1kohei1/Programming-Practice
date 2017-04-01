943d1e074dc2fd608cb715a85b9f282d98db8534
Kohei Arai <tobecomebig@gmail.com>
2017-04-01 07:18:37 -0400

    UVa 11450 top down dp

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,58 @@
 import java.util.*;
 
-// ABC 45-C
-// http://abc045.contest.atcoder.jp/tasks/arc061_a
+// UVa 11450
+// https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=2445
 
 public class Main {
 	
-    static int n;
-    static int[] nums;
+	static int c;
+	static int m;
+	static long[][] memo;
 
 	public static void main (String[] args) throws java.lang.Exception {
 	    Scanner in = new Scanner(System.in);
-	    String s = in.next();
-	    n = s.length();
-	    nums = new int[n];
 	    
-	    for (int i = 0; i < n; i++) {
-	        nums[i] = new Integer(s.substring(i, i + 1));
+	    int numTest = in.nextInt();
+	    ArrayList[] garment = new ArrayList[2];
+	    
+	    while (numTest-- > 0) {
+	    	m = in.nextInt();
+	    	c = in.nextInt();
+	    	
+		    garment = new ArrayList[c];
+		    memo = new long[c][m + 1];
+		    
+		    for (int i = 0; i < c; i++) {
+		    	garment[i] = new ArrayList<Integer>();
+		    	Arrays.fill(memo[i], -1);
+		    	int k = in.nextInt();
+		    	for (int j = 0; j < k; j++) {
+		    		garment[i].add(in.nextInt());
+		    	}
+		    }
+		    
+		    long answer = solve(garment, 0, 0);
+		    System.out.println(answer == -1 ? "no solution" : answer);
 	    }
 	    
-	    long answer = 0;
-	    for (int i = 0; i < (1 << (n - 1)); i++) {
-	        answer += solve(i);
-	    }
-	    
-	    System.out.println(answer);
 	}
 	
-	public static long solve(int a) {
-	    long sofar = 0;
-	    long returnv = 0;
-	    for (int i = 0; i < n; i++) {
-	       // System.out.printf("%d%c", nums[i], (a & (1 << i)) > 0 ? '+' : ' ');
-
-            sofar *= 10;
-            sofar += nums[i];
-	        
-
-	        if ((a & (1 << i)) > 0) {
-	            returnv += sofar;
-	            sofar = 0;
-	        } else {
-	           // sofar *= 10;
-	           // sofar += nums[i];
-	        }
-	    }
-	    returnv += sofar;
-	   // System.out.printf(": returnv: %d\n", returnv);
-	    return returnv;
+	public static long solve(ArrayList[] garment, int index, int usedMoney) {
+		if (usedMoney > m) {
+			return -1;
+		}
+		if (index == c) {
+			return usedMoney;
+		}
+		if (memo[index][usedMoney] >= 0) {
+			return memo[index][usedMoney];
+		}
+		long answer = -1;
+		// Think about buying a garment at index
+		for (int i = 0; i < garment[index].size(); i++) {
+			answer = Math.max(answer, solve(garment, index + 1, usedMoney + (int) garment[index].get(i)));
+		}
+		memo[index][usedMoney] = answer;
+		return answer;
 	}
 }
