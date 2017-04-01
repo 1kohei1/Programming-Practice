1d609d010ff7ca3690954a26a4ba46f2b83cc3a8
Kohei Arai <tobecomebig@gmail.com>
2017-04-01 13:40:50 -0400

    ABC 29-C

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,30 @@
 import java.util.*;
 
-// ABC 33-C
-// http://abc033.contest.atcoder.jp/tasks/abc033_c
+// ABC 29-C
+// http://abc029.contest.atcoder.jp/tasks/abc029_c
 
 public class Main {
 
-	static long MOD = 1000000007;
+	static int n;
 	
 	public static void main (String[] args) throws java.lang.Exception {
 	    Scanner in = new Scanner(System.in);
-	    
-	    String[] s = in.next().split("\\+");
-	    
-	    long answer = 0;
-	    for (int i = 0; i < s.length; i++) {
-	    	if (s[i].length() == 1 && s[i].charAt(0) != '0') {
-	    		answer++;
-	    	} else if (s[i].contains("*") && s[i].indexOf('0') == -1) {
-	    		answer++;
-	    	}
-	    	answer %= MOD;
-	    }
-	    System.out.println(answer);
+
+	    n = in.nextInt();
+	    char[] c = new char[n];
+	    solve(0, c);
+	}
+	
+	public static void solve(int index, char[] c) {
+		if (index == n) {
+			System.out.println(new String(c));
+			return;
+		}
+		c[index] = 'a';
+		solve(index + 1, c);
+		c[index] = 'b';
+		solve(index + 1, c);
+		c[index] = 'c';
+		solve(index + 1, c);
 	}
 }
