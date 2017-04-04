413c82ce0a4dcd353c9af4419e436bdfc740aa12
Kohei Arai <tobecomebig@gmail.com>
2017-04-04 15:08:49 -0400

    ABC 40-C

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,29 @@
 import java.util.*;
  
-// ABC 6-C
-// http://abc006.contest.atcoder.jp/tasks/abc006_3
+// ABC 40-C
+// http://abc040.contest.atcoder.jp/tasks/abc040_c
  
 public class Main {
-
-	static int n;
-	static int m;
-	static boolean answerFound = false;
-	static int a = -1;
-	static int b = -1;
-	static int c = -1;
-	static int[][] dp;
 	
 	public static void main (String[] args) throws java.lang.Exception {
 	    Scanner in = new Scanner(System.in);
 
-	    n = in.nextInt();
-	    m = in.nextInt();
+	    int n = in.nextInt();
+	    int[] nums = new int[n];
 	    
-	    dp = new int[n + 1][m + 1];
-	    solve(0, m, 0, 0, 0);
-	    System.out.printf("%d %d %d\n", a, b, c);
-	}
-	
-	public static void solve(int nn, int mm, int aa, int bb, int cc) {
-		if (nn == n && mm == 0) {
-			answerFound = true;
-			a = aa;
-			b = bb;
-			c = cc;
-		} else if (answerFound || nn > n || mm < 0 || dp[nn][mm] == 1) {
-			return;
-		} else {
-			// child
-			solve(nn + 1, mm - 4, aa, bb, cc + 1);
-			// elder
-			solve(nn + 1, mm - 3, aa, bb + 1, cc);
-			// adult
-			solve(nn + 1, mm - 2, aa + 1, bb, cc);
-			
-			// Don't explore this combination any more
-			dp[nn][mm] = 1;
-		}
+	    for (int i = 0; i < n; i++) {
+	    	nums[i] = in.nextInt();
+	    }
+	    
+	    long[] dp = new long[n];
+	    dp[0] = 0;
+	    dp[1] = Math.abs(nums[0] - nums[1]);
+	    for (int i = 2; i < n; i++) {
+	    	dp[i] = Math.min(
+	    			dp[i - 1] + Math.abs(nums[i] - nums[i - 1]), 
+	    			dp[i - 2] + Math.abs(nums[i] - nums[i - 2])
+	    	);
+	    }
+	    System.out.println(dp[n - 1]);
 	}
 }
