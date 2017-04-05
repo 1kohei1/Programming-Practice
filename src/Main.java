1385107603f0e3007201062e4ab8adbe50615f60
Kohei Arai <tobecomebig@gmail.com>
2017-04-05 10:27:43 -0400

    ABC 21-C

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
  
-// ABC 31-C
-// http://abc031.contest.atcoder.jp/tasks/abc031_c
+// ABC 21-C
+// http://abc021.contest.atcoder.jp/tasks/abc021_c
  
 public class Main {
 	
@@ -9,56 +9,71 @@ public class Main {
 		Scanner in = new Scanner(System.in);
 		
 		int n = in.nextInt();
-		int[] nums = new int[n];
+		int a = in.nextInt() - 1;
+		int b = in.nextInt() - 1;
 		
+		ArrayList[] map = new ArrayList[n];
 		for (int i = 0; i < n; i++) {
-			nums[i] = in.nextInt();
+			map[i] = new ArrayList<Integer>();
 		}
 		
-		int answer = -100000;
-		
-		for (int i = 0; i < n; i++) {
-			answer = Math.max(answer, takahashiScore(nums, i));
+		int m = in.nextInt();
+		for (int i = 0; i < m; i++) {
+			int x = in.nextInt() - 1;
+			int y = in.nextInt() - 1;
+			
+			map[x].add(y);
+			map[y].add(x);
 		}
 		
-		System.out.println(answer);
 		
-		// Iterate through 50 positions that Takahashi  can mark
-		// Figure out which element Aoki will mark
-		// Save the answer
-	}
+		int[] numWay = new int[n]; // numWay[x] stores the number of way to reach x with minimum way
+		int[] dist = new int[n];   // dist[x] stores the minimum path took to reach x
+		int[] visited = new int[n];
+		
+		boolean isAnswerFound = false;
+		
+		// Stores prevNode, currNode, distToCurr
+		ArrayList<Integer> q = new ArrayList<Integer>();
+		q.add(-1);
+		q.add(a);
+		q.add(0);
+		
+		while (q.size() > 0) {
+			int prev = q.remove(0);
+			int curr = q.remove(0);
+			int distToCurr = q.remove(0);
 
-	public static int takahashiScore(int[] nums, int n) {
-		int maxSoFar = -1000000;
-		int takahashiscore = -100;
-		
-		for (int i = 0; i < nums.length; i++) {
-			if (i == n) {
+			// If it is already investigated,
+			if (visited[curr] == 1) {
+				// And if the distance to next is equal to that node
+				if (dist[curr] == distToCurr) {
+					numWay[curr] += numWay[prev];
+					numWay[curr] %= 1000000007;
+				}
 				continue;
 			}
-			int low = Math.min(n,  i);
-			int max = Math.max(n, i);
-
-			int takashi = 0;
-			int aoki = 0;
+			visited[curr] = 1;
+			numWay[curr] = prev == -1 ? 1 : numWay[prev];
+			dist[curr] = distToCurr;
 			
-			// Calculate takashi score and aoki score when they choose the range [i..n] or [n..i]
-			for (int j = low; j <= max; j++) {
-				// Takashi score
-				if ((j - low + 1) % 2 == 1) {
-					takashi += nums[j];
-				} 
-				// Aoki score
-				else {
-					aoki += nums[j];
+			for (int i = 0; i < map[curr].size(); i++) {
+				int next = (int) map[curr].get(i);
+				
+				if (visited[next] == 0) {
+					q.add(curr);
+					q.add(next);
+					q.add(distToCurr + 1);
+				} else if (dist[next] == distToCurr + 1) {
+					System.out.println("come here");
+					numWay[next] += numWay[curr];
+					numWay[curr] %= 1000000007;
 				}
 			}
-			
-			if (maxSoFar < aoki) {
-				maxSoFar = aoki;
-				takahashiscore = takashi;
-			}
 		}
-		return takahashiscore;
+//		for (int i = 0; i < n; i++) {
+//			System.out.printf("%d: %d\n", i, numWay[i]);
+//		}
+		System.out.println(numWay[b]);
 	}
 }
