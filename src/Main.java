a42d7cf2d31082a97cb2e6b6ad84a654c89f2079
Kohei Arai <tobecomebig@gmail.com>
2017-04-06 21:59:12 -0400

    ABC 19-C

--- src/Main.java
+++ src/Main.java
@@ -1,160 +1,45 @@
 import java.util.*;
  
-// ABC 20-C
-// http://abc020.contest.atcoder.jp/tasks/abc020_c
+// ABC 19-C
+// http://abc019.contest.atcoder.jp/tasks/abc019_3
  
 public class Main {
 
-	static int h;
-	static int w;
-	static int t;
-	static char[][] map;
-	
-	static int startr = 0;
-	static int startc = 0;
-	static int goalr = 0;
-	static int goalc = 0;
-	
-	static boolean canReachGoal;
-	static int[] dx = new int[]{-1, 0, 1, 0};
-	static int[] dy = new int[]{0, -1, 0, 1};
-
 	public static void main (String[] args) throws java.lang.Exception {
 		Scanner in = new Scanner(System.in);
-		
-		h = in.nextInt();
-		w = in.nextInt();
-		t = in.nextInt();
-		
-		map = new char[h][w];
-		for (int i = 0; i < h; i++) {
-			map[i] = in.next().toCharArray();
-			for (int j = 0; j < w; j++) {
-				if (map[i][j] == 'S') {
-					startr = i;
-					startc = j;
-				}
-				if (map[i][j] == 'G') {
-					goalr = i;
-					goalc = j;
-				}
-			}
-		}
-		
-		System.out.println(binarysearch());
-	}
-	
-	public static int binarysearch() {
-		int low = 1;
-		int high = t;
-		
-		while (low <= high) {
-			int mid = (low + high) / 2;
-			if (canReachGoalWithX(mid)) {
-				low = mid + 1;
-			} else {
-				high = mid - 1;
-			}
-		}
-		return high;
-	}
 
-	/* Not a good approach. Too much computations
-	public static boolean canReachGoal(int x) {
-		canReachGoal = false;
-		char[][] visited = new char[h][w];
-		visited[startr][startc] = '1';
-		canReachGoalRec(0, x, startr, startc, visited);
-		return canReachGoal ;
-	}
-	*/
-	
-	public static boolean canReachGoalWithX(int x) {
-		PriorityQueue<CustomClass> q = new PriorityQueue<CustomClass>();
-		q.add(new CustomClass(startr, startc, 0));
+		int n = in.nextInt();
 		
-		int[][] visited = new int[h][w];
+		HashMap<Long, Integer> map = new HashMap<Long, Integer>(); 
 		
-		int[][] dist = new int[h][w];
-		for (int i = 0; i < h; i++) {
-			Arrays.fill(dist[i], Integer.MAX_VALUE / 2 - 1);
-		}
-		dist[startr][startc] = 0;
+		int counter = 0;
 		
-		while (q.size() > 0) {
-			CustomClass curr = q.poll();
+		for (int i = 0; i < n; i++) {
+			int x = in.nextInt();
+			long xx = x;
 			
-			if (curr.r == goalr && curr.c == goalc) {
-				break;
-			}
-			if (visited[curr.r][curr.c] == 1) {
-				continue;
-			}
-			
-			visited[curr.r][curr.c] = 1;
-			for (int i = 0; i < 4; i++) {
-				int newr = curr.r + dx[i];
-				int newc = curr.c + dy[i];
-				
-				if (0 <= newr && newr < h && 0 <= newc && newc < w && visited[newr][newc] == 0) {
-					int cost = map[newr][newc] == '#' ? x : 1;
-					
-					if (dist[curr.r][curr.c] + cost < dist[newr][newc]) {
-						dist[newr][newc] = dist[curr.r][curr.c] + cost;
-						q.add(new CustomClass(newr, newc, dist[newr][newc]));
-					}
+			int groupNum = -1;
+			while (xx >= 1) {
+				if (map.containsKey(xx)) {
+					groupNum = map.get(xx);
+					break;
+				} else if (xx % 2 == 0) {
+					xx /= 2;
+				} else {
+					break;
 				}
 			}
-		}
-		return dist[goalr][goalc] <= t;
-		
-	}
-	
-	/* Not a good approach. Too much computations
-	public static void canReachGoalRec(int time, int x, int r, int c, char[][] visited) {
-		
-		if (time > t) {
-			return;
-		}
-		if (r == goalr && c == goalc) {
-			canReachGoal = true;
-			return;
-		}
-		// If we already know we can reach goal, no more investigation
-		if (canReachGoal) {
-			return;
-		}
-		
-		for (int i = 0; i < 4; i++) {
-			int newr = r + dx[i];
-			int newc = c + dy[i];
 			
-			if (0 <= newr && newr < h && 0 <= newc && newc < w && visited[newr][newc] == '\0') {
-				int addt = map[newr][newc] == '#' ? x : 1;
-				visited[newr][newc] = '1';
-				canReachGoalRec(time + addt, x, newr, newc, visited);
-				visited[newr][newc] = '\0';
+			int inserting = groupNum == -1 ? counter : groupNum;
+			while (xx <= x) {
+				map.put(xx, inserting);
+				xx *= 2;
+			}
+			if (groupNum == -1) {
+				counter++;
 			}
 		}
-	}
-	*/
-}
-
-class CustomClass implements Comparable<CustomClass> {
-	int r;
-	int c;
-	int dist;
-	
-	public CustomClass(int r, int c, int dist) {
-		this.r = r;
-		this.c = c;
-		this.dist = dist;
-	}
-	
-	@Override
-	public int compareTo(CustomClass o) {
-		return this.dist - o.dist;
-	}
-	
-	
+		
+		System.out.println(counter);
+	}	
 }
