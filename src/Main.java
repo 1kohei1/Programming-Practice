d40e1aa1fdaed733977dff945c37709414bdfe28
Kohei Arai <tobecomebig@gmail.com>
2017-04-09 00:21:31 -0400

    ABC 5-C

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,44 @@
 import java.util.*;
 import java.awt.Point;
 
-// ABC 15-C
-// http://abc015.contest.atcoder.jp/tasks/abc015_3
+// ABC 5-C
+// http://abc005.contest.atcoder.jp/tasks/abc005_3
  
 public class Main {
-	
-	static int n;
-	static int k;
-	static int[][] nums;
-	static boolean isBug = false;
 	
 	public static void main (String[] args) throws java.lang.Exception {
 		Scanner in = new Scanner(System.in);
 
-		n = in.nextInt();
-		k = in.nextInt();
-		
-		nums = new int[n][k];
+		int t = in.nextInt();
+		int n = in.nextInt();
+		ArrayList<Integer> takos = new ArrayList<Integer>();
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < k; j++) {
-				nums[i][j] = in.nextInt();
+			takos.add(in.nextInt());
+		}
+		int m = in.nextInt();
+		ArrayList<Integer> customers = new ArrayList<Integer>();
+		for (int i = 0; i < m; i++) {
+			customers.add(in.nextInt());
+		}
+		
+		boolean isPossible = true;
+		
+		while (customers.size() > 0 && isPossible) {
+			int time = customers.remove(0);
+			
+			int start = time - t;
+			boolean isTakoyakiSold = false;
+			while (start <= time && !isTakoyakiSold) {
+				if (takos.contains(start)) {
+					int index = takos.indexOf(start);
+					takos.remove(index);
+					isTakoyakiSold = true;
+				}
+				start++;
 			}
+			isPossible = isTakoyakiSold;
 		}
 		
-		for (int i = 0; i < k; i++) {
-			solve(1, nums[0][i]);
-		}
-		
-		System.out.println(isBug ? "Found" : "Nothing");
-	}
-	
-	public static void solve(int index, int xor) {
-		if (isBug) {
-			return;
-		}
-		if (index == n) {
-			if (xor == 0) {
-				isBug = true;
-			}
-			return;
-		}
-		
-		for (int i = 0; i < k; i++) {
-			solve(index + 1, xor ^ nums[index][i]);
-		}
+		System.out.println(isPossible ? "yes" : "no");
 	}
 }
