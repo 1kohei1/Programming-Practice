e6d14f4b033efee4b65f472f6b6768735032ac18
Kohei Arai <tobecomebig@gmail.com>
2017-04-09 00:55:16 -0400

    ABC 2-C

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,34 @@
 import java.util.*;
 import java.awt.Point;
 
-// ABC 5-C
-// http://abc005.contest.atcoder.jp/tasks/abc005_3
+// ABC 2-C
+// http://abc002.contest.atcoder.jp/tasks/abc002_3
  
 public class Main {
 	
 	public static void main (String[] args) throws java.lang.Exception {
 		Scanner in = new Scanner(System.in);
 
-		int t = in.nextInt();
-		int n = in.nextInt();
-		ArrayList<Integer> takos = new ArrayList<Integer>();
-		for (int i = 0; i < n; i++) {
-			takos.add(in.nextInt());
-		}
-		int m = in.nextInt();
-		ArrayList<Integer> customers = new ArrayList<Integer>();
-		for (int i = 0; i < m; i++) {
-			customers.add(in.nextInt());
-		}
+		double x1 = in.nextInt();
+		double y1 = in.nextInt();
+		double x2 = in.nextInt();
+		double y2 = in.nextInt();
+		double x3 = in.nextInt();
+		double y3 = in.nextInt();
 		
-		boolean isPossible = true;
+		double a = length(x1, y1, x2, y2);
+		double b = length(x2, y2, x3, y3);
+		double c = length(x3, y3, x1, y1);
 		
-		while (customers.size() > 0 && isPossible) {
-			int time = customers.remove(0);
-			
-			int start = time - t;
-			boolean isTakoyakiSold = false;
-			while (start <= time && !isTakoyakiSold) {
-				if (takos.contains(start)) {
-					int index = takos.indexOf(start);
-					takos.remove(index);
-					isTakoyakiSold = true;
-				}
-				start++;
-			}
-			isPossible = isTakoyakiSold;
-		}
+		// Hero's formula
+		double p = (a + b + c) / 2;
+		System.out.println(Math.sqrt(p * (p - a) * (p - b) * (p - c)));
+	}
+	
+	public static double length(double x1, double y1, double x2, double y2) {
+		double len1 = Math.abs(x1 - x2);
+		double len2 = Math.abs(y1 - y2);
 		
-		System.out.println(isPossible ? "yes" : "no");
+		return Math.sqrt(Math.pow(len2, 2) + Math.pow(len1, 2));
 	}
 }
