a9b0ec5fc15e29a629e56efdacc19a16756af294
Kohei Arai <tobecomebig@gmail.com>
2017-04-11 17:54:00 -0400

    ABC 1-C

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,86 @@
 import java.util.*;
 import java.awt.Point;
 
-// ABC 2-C
-// http://abc002.contest.atcoder.jp/tasks/abc002_3
+// ABC 1-C
+// http://abc001.contest.atcoder.jp/tasks/abc001_3
  
 public class Main {
 	
 	public static void main (String[] args) throws java.lang.Exception {
 		Scanner in = new Scanner(System.in);
 
-		double x1 = in.nextInt();
-		double y1 = in.nextInt();
-		double x2 = in.nextInt();
-		double y2 = in.nextInt();
-		double x3 = in.nextInt();
-		double y3 = in.nextInt();
+		double deg = in.nextInt() / 10.0;
+		double dis = in.nextInt() / 60.0;
 		
-		double a = length(x1, y1, x2, y2);
-		double b = length(x2, y2, x3, y3);
-		double c = length(x3, y3, x1, y1);
-		
-		// Hero's formula
-		double p = (a + b + c) / 2;
-		System.out.println(Math.sqrt(p * (p - a) * (p - b) * (p - c)));
+		solve(deg, dis);
 	}
 	
-	public static double length(double x1, double y1, double x2, double y2) {
-		double len1 = Math.abs(x1 - x2);
-		double len2 = Math.abs(y1 - y2);
+	public static void solve(double deg, double dis) {
+		String s1 = "";
+		String s2 = "";
+		if (11.25 <= deg && deg < 33.75) {
+			s1 = "NNE";
+		} else if (33.75 <= deg && deg < 56.25) {
+			s1 = "NE";
+		} else if (56.25 <= deg && deg < 78.75) {
+			s1 = "ENE";
+		} else if (78.75 <= deg && deg < 101.25) {
+			s1 = "E";
+		} else if (101.25 <= deg && deg < 123.75) {
+			s1 = "ESE";
+		} else if (123.75 <= deg && deg < 146.25) {
+			s1 = "SE";
+		} else if (146.25 <= deg && deg < 168.75) {
+			s1 = "SSE";
+		} else if (168.75 <= deg && deg < 191.25) {
+			s1 = "S";
+		} else if (191.25 <= deg && deg < 213.75) {
+			s1 = "SSW";
+		} else if (213.75 <= deg && deg < 236.25) {
+			s1 = "SW";
+		} else if (236.25 <= deg && deg < 258.75) {
+			s1 = "WSW";
+		} else if (258.75 <= deg && deg < 281.25) {
+			s1 = "W";
+		} else if (281.25 <= deg && deg < 303.75) {
+			s1 = "WNW";
+		} else if (303.75 <= deg && deg < 326.25) {
+			s1 = "NW";
+		} else if (326.25 <= deg && deg < 348.75) {
+			s1 = "NNW";
+		} else {
+			s1 = "N";
+		}
 		
-		return Math.sqrt(Math.pow(len2, 2) + Math.pow(len1, 2));
+		if (dis < 0.25) {
+			s1 = "C";
+			s2 = "0";
+		} else if (0.25 <= dis && dis < 1.55) {
+			s2 = "1";
+		} else if (1.55 <= dis && dis < 3.25) {
+			s2 = "2";
+		} else if (3.25 <= dis && dis < 5.45) {
+			s2 = "3";
+		} else if (5.45 <= dis && dis < 7.95) {
+			s2 = "4";
+		} else if (7.95 <= dis && dis < 10.75) {
+			s2 = "5";
+		} else if (10.75 <= dis && dis < 13.85) {
+			s2 = "6";
+		} else if (13.85 <= dis && dis < 17.15) {
+			s2 = "7";
+		} else if (17.15 <= dis && dis < 20.75) {
+			s2 = "8";
+		} else if (20.75 <= dis && dis < 24.45) {
+			s2 = "9";
+		} else if (24.45 <= dis && dis < 28.45) {
+			s2 = "10";
+		} else if (28.45 <= dis && dis < 32.65) {
+			s2 = "11";
+		} else {
+			s2 = "12";
+		}
+		
+		System.out.printf("%s %s\n", s1, s2);
 	}
 }
