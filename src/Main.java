dda396b5682d744b21d068eb40a85bd97c0a1df9
Kohei Arai <tobecomebig@gmail.com>
2017-04-17 19:59:58 -0400

    ABC 32-C

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,54 @@
 import java.util.*;
 
-// AGC 13-B
-// http://agc013.contest.atcoder.jp/tasks/agc013_b
+// ABC 32-C
+// http://abc032.contest.atcoder.jp/tasks/abc032_c
  
 public class Main {
-
-	static int n;
-	static ArrayList[] map;
-	static boolean answerFound = false;
-	static ArrayList<Integer> answer;
 
 	public static void main (String[] args) throws java.lang.Exception {
 		Scanner in = new Scanner(System.in);
 		
-		n = in.nextInt();
-		int m = in.nextInt();
+		int n = in.nextInt();
+		int k = in.nextInt();
 		
-		map = new ArrayList[n];
+		int[] nums = new int[n];
+		boolean zeroFlag = false;
+		
 		for (int i = 0; i < n; i++) {
-			map[i] = new ArrayList<Integer>();
-		}
-		
-		for (int i = 0; i < m; i++) {
-			int a = in.nextInt() - 1;
-			int b = in.nextInt() - 1;
-			
-			map[a].add(b);
-			map[b].add(a);
-		}
-
-		int index = 0;
-		while (map[index].size() == 0) {
-			index++;
-		}
-		int node1 = index;
-		int node2 = (int) map[index].get(0);
-		
-		int[] visited = new int[n];
-		ArrayList<Integer> path = new ArrayList<Integer>();
-		visited[node1] = 1;
-		visited[node2] = 1;
-		path.add(node1);
-		dfs(node1, path, visited, true);
-		path.add(node2);
-		dfs(node2, path, visited, false);
-		
-		System.out.println(path.size());
-		for (int i = 0; i < path.size(); i++) {
-			System.out.printf("%d ", path.get(i) + 1);
-		}
-		System.out.println();
-	}
-	
-	public static void dfs(int node, ArrayList<Integer> path, int[] visited, boolean insertFront) {
-		for (int i = 0; i < map[node].size(); i++) {
-			int next = (int) map[node].get(i);
-			
-			if (visited[next] == 0) {
-				visited[next] = 1;
-				if (insertFront) {
-					path.add(0, next);
-				} else {
-					path.add(next);
-				}
-				dfs(next, path, visited, insertFront);
-				break;
+			nums[i] = in.nextInt();
+			if (nums[i] == 0) {
+				zeroFlag = true;
 			}
+		}
+		
+		if (zeroFlag) {
+			System.out.println(n);
+		} else {
+			int start = 0;
+			int end = 0;
+			
+			long multi = 1;
+			int answer = 0;
+			
+			while (end < n) {
+				// Extend end as long as the multi is less than equal to k
+				while (end < n && multi * nums[end] <= k) {
+					multi *= nums[end];
+					end++;
+				}
+				answer = Math.max(answer, end - start);
+				
+				if (start == end) {
+					start++;
+					end++;
+					multi = 1;
+				} else {
+					multi /= nums[start];
+					start++;
+				}
+			}
+			
+			System.out.println(answer);
 		}
 	}
 }
