4bbe103ebdd20b277cb6ce0361f2722b789d2bcb
Kohei Arai <tobecomebig@gmail.com>
2017-04-22 10:42:57 -0400

    ABC 59-C

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,48 @@
 import java.util.*;
 
-// ABC 6-C
-// http://abc006.contest.atcoder.jp/tasks/abc006_3
+// ABC 59-C
+// http://abc059.contest.atcoder.jp/tasks/arc072_a
  
 public class Main {
 	
 	public static void main (String[] args) throws java.lang.Exception {
 		Scanner in = new Scanner(System.in);
-		
+
 		int n = in.nextInt();
-		int m = in.nextInt();
 		
-		if (n * 4 < m || m < 2 * n) {
-			System.out.println("-1 -1 -1");
+		int[] nums = new int[n];
+		for (int i = 0; i < n; i++) {
+			nums[i] = in.nextInt();
+		}
+		
+		long answer = solve(nums, 0, 0);
+		if (nums[0] > 0) {
+			answer = Math.min(solve(nums, nums[0], 1), nums[0] + 1 + solve(nums, -1, 1));
+		} else if (nums[0] < 0) {
+			answer = Math.min(solve(nums, nums[0], 1), Math.abs(nums[0]) + 1 + solve(nums, 1, 1));
 		} else {
-			int a = 0, b = 0, c = 0; // a: 大人, b: 老人, c: 赤ちゃん
-			int X = 4 * n - m;
-			int Y = X / 2;
-			int Z = (X + 1) / 2;
-			
-			System.out.printf("%d %d %d\n", Y, X % 2 == 0 ? 0 : 1, n - Z);
+			answer = Math.min(1 + solve(nums, 1, 1), 1 + solve(nums, -1, 1));
+		}
+		
+		System.out.println(answer);
+	}
+	
+	public static long solve(int[] nums, long sum, int index) {
+		if (index == nums.length) {
+			return 0;
+		}
+		if (sum < 0 && sum + nums[index] < 0) {
+			return 1 + Math.abs(sum + nums[index]) + solve(nums, 1, index + 1);
+		} else if (sum > 0 && sum + nums[index] > 0) {
+			return 1 + sum + nums[index] + solve(nums, -1, index + 1);
+		} else if (sum + nums[index] == 0) {
+			if (sum > 0) {
+				return 1 + solve(nums, -1, index + 1);
+			} else {
+				return 1 + solve(nums, 1, index + 1);
+			}
+		} else {
+			return solve(nums, sum + nums[index], index + 1);
 		}
 	}
 }
