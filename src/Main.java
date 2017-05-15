2738006f50a13dd5ae4d2e336fe08f9afa33f6fc
Kohei Arai <tobecomebig@gmail.com>
2017-05-14 18:19:38 -0700

    ABC 3-C

--- src/Main.java
+++ src/Main.java
@@ -1,88 +1,36 @@
 import java.util.*;
 
-// ABC 27-C
-// http://abc027.contest.atcoder.jp/tasks/abc027_c
+// ABC 3-C
+// http://abc003.contest.atcoder.jp/tasks/abc003_3
  
 public class Main {
 	
 	public static void main (String[] args) throws java.lang.Exception {
 		Scanner in = new Scanner(System.in);
 		
-		long n = in.nextLong();
-		solve(n);
-	}
-	
-	public static void solve(long n) {
-		String winner = "";
+		int n = in.nextInt();
+		int k = in.nextInt();
 		
-		if (n == 1) {
-			winner = "Aoki";
-		} else if (n == 2 || n == 3) {
-			winner = "Takahashi";
-		} else {
-			int numBit = sigBit(n);
-			boolean isInArow = numBit % 2 == 0;
-			long left = (long) 1 << numBit;
-			long right = (long) 1 << (numBit + 1);
-			
-			long zigzag = zigzag(numBit, isInArow);
-			
-			if (isInArow) {
-				if (left <= n && n < zigzag) {
-					winner = "Takahashi";
-				} else {
-					winner = "Aoki";
-				}
-			} else {
-				if (zigzag <= n && n < right) {
-					winner = "Takahashi";
-				} else {
-					winner = "Aoki";
-				}
-			}
+		int[] nums = new int[n];
+		for (int i = 0; i < n; i++) {
+			nums[i] = in.nextInt();
 		}
 		
-		System.out.println(winner);
-	}
-	
-	public static long zigzag(int numBit, boolean isInArow) {
-		long returnv = 1;
-		for (int i = 1; i <= numBit; i++) {
-			// We can shorten this code, but to make the logic easy to understand, I leave this in redundant way.
-			// If it is in A row, A always go to right and B always go to left
-			if (isInArow) {
-				// This is A action. Go right
-				if (i % 2 == 1) {
-					returnv *= 2;
-					returnv++;
-				}
-				// This is B action. Go left
-				else {
-					returnv *= 2;
-				}
+		Arrays.sort(nums);
+		double answer = 0;
+		int index = n - 1;
+		
+		for (int i = k - 1; i >= 0; i--) {
+			int counter = 0;
+			double temp = nums[index];
+			while (counter < k - i) {
+				temp /= 2.0;
+				counter++;
 			}
-			// If it is in B row, A always go to left and B always go to right
-			else {
-				// This is A action. Go left
-				if (i % 2 == 1) {
-					returnv *= 2;
-				}
-				// This is B action. Go right
-				else {
-					returnv *= 2;
-					returnv++;
-				}
-			}
+			answer += temp;
+			index--;
 		}
-		return returnv;
-	}
-	
-	public static int sigBit(long a) {
-		int num = 0;
-		while ((a >> 1) > 0) {
-			a = a >> 1;
-			num++;
-		}
-		return num;
+		
+		System.out.println(answer);
 	}
 }
