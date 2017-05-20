a75393868c3592c6a55c40ccc3225c94b4ade9d1
Kohei Arai <tobecomebig@gmail.com>
2017-05-20 08:58:50 -0400

    ABC 62-C

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,51 @@
 import java.util.*;
 
-// ABC 18-C
-// http://abc018.contest.atcoder.jp/tasks/abc018_3
+// ABC 62-C
+// http://abc062.contest.atcoder.jp/tasks/arc074_a
  
 public class Main {
 
-	static int R;
-	static int C;
-	static int K;
-	static char map[][];
-	
 	public static void main (String[] args) throws java.lang.Exception {
 		Scanner in = new Scanner(System.in);
+		
+		int h = in.nextInt();
+		int w = in.nextInt();
 
-		R = in.nextInt();
-		C = in.nextInt();
-		K = in.nextInt();
-		
-		map = new char[R][C];
-		for (int i = 0; i < R; i++) {
-			map[i] = in.next().toCharArray();
-		}
-		
-		int[][] whiteInRowAbove = new int[R][C];
-		int[][] whiteInRowBottom = new int[R][C];
-		
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				if (map[i][j] == 'x') {
-					whiteInRowAbove[i][j] = 0;
-					whiteInRowBottom[i][j] = 0;
-				} else {
-					int count = 1;
-					int row = i - 1;
-					while (0 <= row && map[row][j] == 'o' && count <= K) {
-						count++;
-						row--;
-					}
-					whiteInRowAbove[i][j] = count;
-					
-					count = 1;
-					row = i + 1;
-					while (row < R && map[row][j] == 'o' && count <= K) {
-						count++;
-						row++;
-					}
-					
-					whiteInRowBottom[i][j] = count;
-				}
-			}
+		long answer = Long.MAX_VALUE;
+
+		// Divide vertically
+		int dividedBy3 = w / 3;
+		if (w % 3 == 0) {
+			answer = 0;
+		} else {
+			answer = Math.min(answer, h);
 		}
 
-		long answer = 0;
-		int d = K - 1;
-
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				if (i - d < 0 || i + d >= R || j - d < 0 || j + d >= C) {
-					continue;
-				}
-				
-				boolean shouldContinue = true;
-				
-				for (int k = j - d; k <= j + d && shouldContinue; k++) {
-					int diff = K - Math.abs(j - k);
-					if (whiteInRowAbove[i][k] < diff || whiteInRowBottom[i][k] < diff) {
-						shouldContinue = false;
-					}
-				}
-
-				if (shouldContinue) {
-					answer++;
-				}
-			}
+		// Divide vertically
+		dividedBy3 = h / 3;
+		if (h % 3 == 0) {
+			answer = 0;
+		} else {
+			answer = Math.min(answer, w);
 		}
-
+		
+		// Divide vertically first and divide horizontally in the first column
+		long half = w / 2;
+		for (long i = 1; i < h; i++) {
+			long max = Math.max(Math.max(half * i, (w - half) * i), w * (h - i));
+			long min = Math.min(Math.min(half * i, (w - half) * i), w * (h - i));
+			answer = Math.min(answer, max - min);
+		}
+		
+		// Divide horizontally first and divide vertically in the first row
+		half = h / 2;
+		for (long i = 1; i < w; i++) {
+			long max = Math.max(Math.max(half * i, (h - half) * i), (w - i) * h);
+			long min = Math.min(Math.min(half * i, (h - half) * i), (w - i) * h);
+			
+			answer = Math.min(answer, max - min);
+		}
+		
 		System.out.println(answer);
 	}
 }
