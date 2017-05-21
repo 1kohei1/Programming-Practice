75eaf67953c2b59f69e4b911bd769b22bd14bfe9
Kohei Arai <tobecomebig@gmail.com>
2017-05-21 06:23:41 -0400

    Partial ABC 17-D

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,65 @@
 import java.util.*;
 
-// ABC 62-D
-// http://abc062.contest.atcoder.jp/tasks/arc074_b
+// ABC 17-D
+// http://abc017.contest.atcoder.jp/tasks/abc017_3
  
 public class Main {
+	
+	static int N;
+	static int M;
+	static int[] scores;
+	static int[] jewelsLeft;
+	static int[] jewelsRight;
+	
+	static int answer = Integer.MIN_VALUE;
 
 	public static void main (String[] args) throws java.lang.Exception {
 		Scanner in = new Scanner(System.in);
 		
-		int N = in.nextInt();
-		int[] nums = new int[3 * N];
-		for (int i = 0; i < 3 * N; i++) {
-			nums[i] = in.nextInt();
-		}
+		N = in.nextInt();
+		M = in.nextInt();
 		
-		// Do left 
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-		long sum = 0;
+		jewelsLeft = new int[N];
+		jewelsRight = new int[N];
+		scores = new int[N];
+		
 		for (int i = 0; i < N; i++) {
-			pq.add(nums[i]);
-			sum += nums[i];
+			jewelsLeft[i] = in.nextInt();
+			jewelsRight[i] = in.nextInt();
+			scores[i] = in.nextInt();
 		}
 		
-		long[] maxInLeft = new long[N + 1];
-		maxInLeft[0] = sum;
-		
-		for (int i = N; i < 2 * N; i++) {
-			pq.add(nums[i]);
-			sum += nums[i];
-			sum -= pq.poll();
-			maxInLeft[i - N + 1] = sum;
-		}
-		
-		
-		// Do right
-		long[] minInRight = new long[N + 1];
-		sum = 0;
-		pq = new PriorityQueue<Integer>(Collections.reverseOrder());
-		for (int i = 3 * N - 1; i >= 2 * N; i--) {
-			sum += nums[i];
-			pq.add(nums[i]);
-		}
-		
-		minInRight[N] = sum;
-		
-		for (int i = 2 * N - 1; i >= N; i--) {
-			pq.add(nums[i]);
-			sum += nums[i];
-			sum -= pq.poll();
-			minInRight[i - N] = sum;
-		}
-		
-		
-		// Calculate max in left and min in right
-		long answer = Long.MIN_VALUE;
-		for (int i = 0; i < N + 1; i++) {
-			answer = Math.max(answer, maxInLeft[i] - minInRight[i]);
-		}
+		solve(0, new int[M + 1], 0);
 		System.out.println(answer);
+	}
+
+	public static void solve(int index, int[] jewels, int score) {
+		if (index == N) {
+			// Check if all of them are more than or equal to 0
+			boolean isDaemonRevival = true;
+			for (int i = 1; i <= M; i++) {
+				if (jewels[i] < 1) {
+					isDaemonRevival = false;
+					break;
+				}
+			}
+			
+			if (isDaemonRevival) {
+				score = 0;
+			}
+			answer = Math.max(answer, score);
+		} else {
+			// Don't go this heritage
+			solve(index + 1, jewels, score);
+			// Go this heritage
+			for (int i = jewelsLeft[index]; i <= jewelsRight[index]; i++) {
+				jewels[i]++;
+			}
+			solve(index + 1, jewels, score + scores[index]);
+			// Reset jewels
+			for (int i = jewelsLeft[index]; i <= jewelsRight[index]; i++) {
+				jewels[i]--;
+			}
+}
 	}
 }
