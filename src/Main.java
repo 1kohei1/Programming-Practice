d49f0341ff4bab89a0814bf63af480521a714782
Kohei Arai <tobecomebig@gmail.com>
2017-05-21 22:46:05 -0400

    ABC 14-C

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,25 @@
 import java.util.*;
 
-// ABC 17-D
-// http://abc017.contest.atcoder.jp/tasks/abc017_3
- 
 public class Main {
 	
-	static int N;
-	static int M;
-	static int[] scores;
-	static int[] jewelsLeft;
-	static int[] jewelsRight;
-	
-	static int answer = Integer.MIN_VALUE;
-
 	public static void main (String[] args) throws java.lang.Exception {
 		Scanner in = new Scanner(System.in);
 		
-		N = in.nextInt();
-		M = in.nextInt();
+		int n = in.nextInt();
+		int MAX = 1000001;
+		int[] colors = new int[MAX + 1];
 		
-		jewelsLeft = new int[N];
-		jewelsRight = new int[N];
-		scores = new int[N];
-		
-		for (int i = 0; i < N; i++) {
-			jewelsLeft[i] = in.nextInt();
-			jewelsRight[i] = in.nextInt();
-			scores[i] = in.nextInt();
+		for (int i = 0; i < n; i++) {
+			colors[in.nextInt()]++;
+			colors[in.nextInt() + 1]--;
 		}
 		
-		solve(0, new int[M + 1], 0);
+		int answer = colors[0];
+		
+		for (int i = 1; i < MAX; i++) {
+			colors[i] += colors[i - 1];
+			answer = Math.max(answer, colors[i]);
+		}
 		System.out.println(answer);
-	}
-
-	public static void solve(int index, int[] jewels, int score) {
-		if (index == N) {
-			// Check if all of them are more than or equal to 0
-			boolean isDaemonRevival = true;
-			for (int i = 1; i <= M; i++) {
-				if (jewels[i] < 1) {
-					isDaemonRevival = false;
-					break;
-				}
-			}
-			
-			if (isDaemonRevival) {
-				score = 0;
-			}
-			answer = Math.max(answer, score);
-		} else {
-			// Don't go this heritage
-			solve(index + 1, jewels, score);
-			// Go this heritage
-			for (int i = jewelsLeft[index]; i <= jewelsRight[index]; i++) {
-				jewels[i]++;
-			}
-			solve(index + 1, jewels, score + scores[index]);
-			// Reset jewels
-			for (int i = jewelsLeft[index]; i <= jewelsRight[index]; i++) {
-				jewels[i]--;
-			}
-}
 	}
 }
