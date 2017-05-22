7b37fe48012aa4fb48463e7061f782c96f5e614f
Kohei Arai <tobecomebig@gmail.com>
2017-05-22 00:13:22 -0400

    Partial(100) ABC 17-C

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,45 @@
 import java.util.*;
 
+// ABC 17-C
+// http://abc017.contest.atcoder.jp/tasks/abc017_3
+
 public class Main {
 	
-	public static void main (String[] args) throws java.lang.Exception {
+	static int N;
+	static int M;
+	static int[] jewelStart;
+	static int[] jewelEnd;
+	static int[] scores;
+	
+	public static void main (String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		int n = in.nextInt();
-		int MAX = 1000001;
-		int[] colors = new int[MAX + 1];
+		N = in.nextInt();
+		M = in.nextInt();
 		
-		for (int i = 0; i < n; i++) {
-			colors[in.nextInt()]++;
-			colors[in.nextInt() + 1]--;
+		jewelStart = new int[N];
+		jewelEnd = new int[N];
+		scores = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			jewelStart[i] = in.nextInt();
+			jewelEnd[i] = in.nextInt();
+			scores[i] = in.nextInt();
 		}
 		
-		int answer = colors[0];
+		long answer = 0;
 		
-		for (int i = 1; i < MAX; i++) {
-			colors[i] += colors[i - 1];
-			answer = Math.max(answer, colors[i]);
+		for (int i = 1; i <= M; i++) {
+			long num = 0;
+			for (int j = 0; j < N; j++) {
+				if (!(jewelStart[j] <= i && i <= jewelEnd[j])) {
+					num += scores[j];
+				}
+			}
+			answer = Math.max(answer, num);
 		}
+		
 		System.out.println(answer);
 	}
+	
 }
