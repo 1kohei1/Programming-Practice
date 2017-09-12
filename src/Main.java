0ee7dedd16c4d6bf6af9043ee16c1f328d1d073f
Kohei Arai <tobecomebig@gmail.com>
2017-09-11 23:46:34 -0400

    ARC 82-D

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,45 @@
 import java.util.*;
 
-// ARC 82-C
-// https://beta.atcoder.jp/contests/arc082/tasks/arc082_a
+// ARC 82-D
+// https://beta.atcoder.jp/contests/arc082/tasks/arc082_b
 
 public class Main {
+	
+	static int N;
+	static int answer = Integer.MAX_VALUE;
 
 	public static void main (String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		int N = in.nextInt();
-
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		N = in.nextInt();
+		int[] nums = new int[N];
 		
 		for (int i = 0; i < N; i++) {
-			int n = in.nextInt();
-			
-			if (map.containsKey(n - 1)) {
-				int count = map.get(n - 1);
-				map.put(n - 1, count + 1);
-			} else {
-				map.put(n - 1, 1);
-			}
-			if (map.containsKey(n)) {
-				int count = map.get(n);
-				map.put(n, count + 1);
-			} else {
-				map.put(n, 1);
-			}
-			if (map.containsKey(n + 1)) {
-				int count = map.get(n + 1);
-				map.put(n + 1, count + 1);
-			} else {
-				map.put(n + 1, 1);
-			}
+			nums[i] = in.nextInt();
 		}
 		
 		int answer = 0;
 		
-		for (int k : map.keySet()) {
-			answer = Math.max(answer, map.get(k));
+		for (int i = 0; i < N; i++) {
+			if (i + 1 == nums[i]) {
+				// Not the end of the array
+				if (i != N - 1) {
+					swap(nums, i, i + 1);
+				} else {
+					swap(nums, i, i - 1);
+				}
+				answer++;
+			}
 		}
 		
 		System.out.println(answer);
+	}
+	
+	
+	public static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 }
 
