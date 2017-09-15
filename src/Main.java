159b7429e289d27c3145d5e5418f441054b572d0
Kohei Arai <tobecomebig@gmail.com>
2017-09-15 00:22:58 -0400

    ARC 80-C

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,35 @@
 import java.util.*;
 
-// ARC 81-D
-// https://beta.atcoder.jp/contests/arc081/tasks/arc081_b
+// ARC 80-C
+// http://arc080.contest.atcoder.jp/tasks/arc080_a
 
 public class Main {
 	
-	public static void main (String[] args) {
+	public static void main (String[] args) throws InterruptedException {
 		Scanner in = new Scanner(System.in);
 		
 		int N = in.nextInt();
-		char[][] map = new char[2][52];
+		int num4 = 0;
+		int num2 = 0;
 		
-		map[0] = in.next().toCharArray();
-		map[1] = in.next().toCharArray();
-		
-		long answer = 1;
-		long MOD = 1000000007;
-		int i = 0;
-		
-		// Placed vertically
-		if (map[0][0] == map[1][0]) {
-			answer = 3;
-			i = 1;
-		}
-		// Placed horizontally
-		else {
-			answer = 6;
-			i = 2;
+		for (int i = 0; i < N; i++) {
+			int n = in.nextInt();
+			if (n % 4 == 0) {
+				num4++;
+			} else if (n % 2 == 0) {
+				num2++;
+			}
 		}
 		
-		for (; i < N; i++) {
-			// Placed vertically
-			if (map[0][i] == map[1][i]) {
-				// Previous domino placed vertically
-				if (map[0][i - 1] == map[1][i - 1]) {
-					answer *= 2;
-				}
-				// Previous domino placed horizontally
-				else {
-					answer *= 1;
-				}
+		String answer = "No";
+		
+		if (num4 >= N / 2) {
+			answer = "Yes";
+		} else {
+			int remain = N - num4 * 2;
+			if (remain <= num2) {
+				answer = "Yes";
 			}
-			// Placed horizontally
-			else {
-				// Previous domino placed vertically
-				if (map[0][i - 1] == map[1][i - 1]) {
-					answer *= 2;
-				}
-				// Previous domino placed horizontally
-				else {
-					answer *= 3;
-				}
-				i++;
-			}
-			answer %= MOD;
 		}
 		
 		System.out.println(answer);
