fbd4baf3c68f57a020f34f18b4c6cf4c74ff65d7
Kohei Arai <tobecomebig@gmail.com>
2017-09-16 08:38:30 -0400

    ARC 83-C

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,81 @@
 import java.util.*;
 
-// ARC 79-C
-// http://arc079.contest.atcoder.jp/tasks/arc079_a
+// ARC 83-C
+// http://arc083.contest.atcoder.jp/tasks/arc083_a
 
 public class Main {
+	
+	static int A;
+	static int B;
+	static int C;
+	static int D;
+	static int E;
+	static int F;
+	
+	static double concentration = 0;
+	static int answerW = 0;
+	static int answerS = 0;
+	
+	static int[][] dp;
 	
 	public static void main (String[] args) throws InterruptedException {
 		Scanner in = new Scanner(System.in);
 		
-		int N = in.nextInt();
-		int M = in.nextInt();
+		A = in.nextInt();
+		B = in.nextInt();
+		C = in.nextInt();
+		D = in.nextInt();
+		E = in.nextInt();
+		F = in.nextInt();
 		
-		Set<Integer> from1 = new TreeSet<Integer>();
-		Set<Integer> toN = new TreeSet<Integer>();
+		dp = new int[F + 1][F * E / 100 + 1];
 		
-		for (int i = 0; i < M; i++) {
-			int s = in.nextInt();
-			int g = in.nextInt();
-			
-			if (s == 1 && !from1.contains(g)) {
-				from1.add(g);
-			} else if (g == N && !toN.contains(s)) {
-				toN.add(s);
-			}
+		answerW = 100 * A;
+		
+		solve(0, 0);
+		
+		System.out.printf("%d %d\n", answerW + answerS, answerS);
+	}
+	
+	public static void solve(int water, int sugar) {
+		if (water + sugar > F || water > F || sugar > F * E / 100) {
+			return;
+		}
+		if (dp[water][sugar] == 1) {
+			return;
 		}
 		
-		String answer = "IMPOSSIBLE";
+		boolean isSugarDissolved = isSugarDissolved(water, sugar);
+		if (isSugarDissolved) {
+			double conc = calculateConcentration(water, sugar);
+			if (conc > concentration) {
+				concentration = conc;
+				answerW = water;
+				answerS = sugar;
+			}
+		}
 
-		for (int island: from1) {
-			if (toN.contains(island)) {
-				answer = "POSSIBLE";
-				break;
-			}
-		}
+		dp[water][sugar] = 1;
 				
-		System.out.println(answer);
+		// Operation 1
+		solve(water + 100 * A, sugar);
+		// Operation 2
+		solve(water + 100 * B, sugar);
+		
+		// Only sugar is dissolved, check adding sugar
+		if (isSugarDissolved) {
+			// Operation 3
+			solve(water, sugar + C);
+			// Operation 4
+			solve(water, sugar + D);
+		}
+	}
+	
+	public static double calculateConcentration(int water, int sugar) {
+		return (100.0 * sugar) / (water + sugar);
+	}
+	
+	public static boolean isSugarDissolved(int water, int sugar) {
+		return sugar <= water * E / 100;
 	}
 }
