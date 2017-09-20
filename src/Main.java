1d75e6185e18a32affe131a1f7f0523432d3ad07
Kohei Arai <tobecomebig@gmail.com>
2017-09-20 12:16:04 -0400

    ARC 57-A

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,46 @@
 import java.util.*;
 
-// CF 435-A
-// http://codeforces.com/contest/862/problem/A
+// ARC 57-A
+// http://arc057.contest.atcoder.jp/tasks/arc057_a
 
 public class Main {
 	
 	public static void main (String[] args) throws InterruptedException {
 		Scanner in = new Scanner(System.in);
 		
-		int n = in.nextInt();
-		int x = in.nextInt();
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		long A = in.nextLong();
+		long K = in.nextLong();
+		long nichoh = new Long("2000000000000");
 		
-		for (int i = 0; i < n; i++) {
-			int a = in.nextInt();
-			map.put(a,  1);
+		if (K == 0) {
+			System.out.println(nichoh - A);
+			return;
 		}
 		
-		int numOp = 0;
-		for (int i = 0; i < x; i++) {
-			if (!map.containsKey(i)) {
-				numOp++;
+		long low = 0;
+		long high = new Long("2000000000000");
+		
+		while (low <= high) {
+			long mid = (low + high) / 2;
+			long k = mid - 1;
+			
+			long money = (long) ((long) A * Math.pow(K + 1, k));
+			k--;
+			while (k >= 0 && money < nichoh) {
+				money += (long) Math.pow(K + 1, k);
+				k--;
+			}
+			
+//			System.out.printf("low: %d, high: %d, mid: %d, money: %d\n", low, high, mid, money);
+			
+			if (money >= nichoh) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
 			}
 		}
-		if (map.containsKey(x)) {
-			numOp++;
-		}
 		
-		System.out.println(numOp);
+		System.out.println(high);
 	}
+	
 }
