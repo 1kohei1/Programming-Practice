b4730bf1d91b4c5874d5edc3eb3c42dd55b91539
Kohei Arai <tobecomebig@gmail.com>
2017-09-23 09:24:18 -0400

    CODEFES 2017.qualA-C

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,75 @@
 import java.util.*;
 
-// ARC 77-C
-// http://arc077.contest.atcoder.jp/tasks/arc077_a
+// CODEFES 2017.qualA-C
+// http://code-festival-2017-quala.contest.atcoder.jp/tasks/code_festival_2017_quala_c
 
 public class Main {
 	
 	public static void main (String[] args) throws InterruptedException {
 		Scanner in = new Scanner(System.in);
 		
-		int N = in.nextInt();
-		int[] nums = new int[N];
+		int H = in.nextInt();
+		int W = in.nextInt();
 		
-		for (int i = 0; i < N; i++) {
-			nums[i] = in.nextInt();
+		int[] freq = new int[26];
+		for (int i = 0; i < H; i++) {
+			char[] c = in.next().toCharArray();
+			for (int j = 0; j < c.length; j++) {
+				freq[c[j] - 'a']++;
+			}
 		}
 		
-		int index = N - 1;
-		StringBuilder s = new StringBuilder();
-		while (index >= 0) {
-			s.append(nums[index] + " ");
-			index -= 2;
+		if (H == 1 || W == 1) {
+			int numRemaining = 0;
+			int index = 0;
+			while (index < 26) {
+				if (freq[index] >= 2) {
+					freq[index] -= 2;
+				} else {
+					numRemaining += freq[index];
+					index++;
+				}
+			}
+			
+			System.out.println(numRemaining == Math.max(H,  W) % 2 ? "Yes" : "No");
+		} else {
+			int num2Ok = 0;
+			int num1Ok = 0;
+			
+			if (H % 2 == 1) {
+				num2Ok += W / 2;
+			}
+			if (W % 2 == 1) {
+				num2Ok += H / 2;
+			}
+			if (H % 2 == 1 && W % 2 == 1) {
+				num1Ok = 1;
+			}
+			
+//			System.out.printf("num2Ok: %d, num1Ok: %d\n", num2Ok, num1Ok);
+			
+			int index = 0;
+			boolean answer = true;
+			while (index < 26 && answer) {
+//				System.out.printf("index: %d, freq[index]: %d\n", index, freq[index]);
+				if (freq[index] >= 4) {
+					freq[index] -= 4;
+				} else if (freq[index] == 2 && num2Ok > 0) {
+					freq[index] -= 2;
+					num2Ok--;
+					index++;
+				} else if (freq[index] == 1 && num1Ok > 0) {
+					freq[index] -= 1;
+					num1Ok--;
+					index++;
+				} else if (freq[index] == 0) {
+					index++;
+				} else {
+					answer = false;
+				}
+			}
+			System.out.println(answer ? "Yes" : "No");
 		}
-		index = N % 2;
-		while (index < N) {
-			s.append(nums[index] + " ");
-			index += 2;
-		}
-		System.out.println(s);
 	}
 	
 }
