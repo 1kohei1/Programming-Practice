6bd31f67aaa12cb47361c9402dec720539aa298f
Kohei Arai <tobecomebig@gmail.com>
2017-09-24 15:02:50 -0400

    Yuki 131

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-// Yuki 208
-// https://yukicoder.me/problems/no/208
+// Yuki 141
+// https://yukicoder.me/problems/no/131
 
 public class Main {
 	
@@ -10,12 +10,19 @@ public class Main {
 		
 		int x = in.nextInt();
 		int y = in.nextInt();
-		int x1 = in.nextInt();
-		int y1 = in.nextInt();
+		int d = in.nextInt();
 		
-		long answer = Math.max(x, y);
-		if (x1 < x && y1 < y && x == y && x1 == y1) {
-			answer++;
+		int answer = 0;
+		if (d <= y) {
+			answer = Math.min(d,  x) + 1;
+		} else if (d <= x) {
+			answer = Math.min(d, y) + 1;
+		} else {
+			int crossX = d - y;
+			if (0 <= crossX && crossX <= x) {
+				int crossY = 0 - x + d;
+				answer = y - crossY + 1;
+			}
 		}
 		System.out.println(answer);
 	}
