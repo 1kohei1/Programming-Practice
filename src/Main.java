b80f062412836311d681a04d01588e6bcd2ed2d8
Kohei Arai <tobecomebig@gmail.com>
2017-10-01 00:42:01 -0400

    Cleaner Tenkaichi 2017-D

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,47 @@
 import java.util.*;
 
-// Tenakaichi 2017-C
-// http://tenka1-2017.contest.atcoder.jp/tasks/tenka1_2017_c	
+// Tenakaichi 2017-D
+// http://tenka1-2017.contest.atcoder.jp/tasks/tenka1_2017_d	
 
 public class Main {
-	
+
 	public static void main (String[] args) throws InterruptedException {
 		Scanner in = new Scanner(System.in);
+
+		int N = in.nextInt();
+		int K = in.nextInt();
+
+		int[] nums = new int[N];
+		int[] values = new int[N];
 		
-		double E = 1e-9;
+		for (int i = 0; i < N; i++) {
+			nums[i] = in.nextInt();
+			values[i] = in.nextInt();
+		}
+
+		long answer = 0;
 		
-		double N = in.nextDouble();
-		boolean answerFound = false;
-		double d = 4 / N;
-		
-		for (double i = 3500; i > 0 && 1 / i < d && !answerFound; i--) {
-			double dd = d - 1 / i;
-			
-			for (double j = 3500; j > 0 && 1 / j < dd && !answerFound; j--) {
-				double ddd = dd - 1 / j;
-				
-				double x = 1 / ddd;
-				int k = 0;
-				
-				if (Math.abs(Math.ceil(x) - x) <= E) {
-					k = (int) Math.ceil(x);
-					answerFound = true;
-				} else if (Math.abs(Math.floor(x) - x) <= E) {
-					k = (int) Math.floor(x);
-					answerFound = true;
+		for (int i = -1; i < 32; i++) {
+			if (i == -1 || ((1 << i) & K) > 0) {
+				long tempAnswer = 0;
+				for (int j = 0; j < N; j++) {
+					if (i == -1 || (nums[j] & (1 << i)) == 0) {
+						boolean shouldAdd = true;
+						for (int k = i + 1; k < 32; k++) {
+							if ((K & (1 << k)) == 0 && (nums[j] & (1 << k)) > 0) {
+								shouldAdd = false;
+								break;
+							}
+						}
+						
+						if (shouldAdd) {
+							tempAnswer += values[j];
+						}
+					}
 				}
-				
-				if (answerFound) {
-					System.out.printf("%d %d %d\n", (int) i, (int) j, k);
-				}
+				answer = Math.max(answer, tempAnswer);
 			}
 		}
+		System.out.println(answer);
 	}
-	
-	
 }
