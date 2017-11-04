1322a213f22a40adcdcf2b76ec5565df2d5242e7
Kohei Arai <tobecomebig@gmail.com>
2017-11-04 14:56:28 -0400

    ARC 84-C

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-//
-//
+// ARC 84-A
+// https://beta.atcoder.jp/contests/arc084/tasks/arc084_a
 
 public class Main {
 	
@@ -10,34 +10,37 @@ public class Main {
 
 		int N = in.nextInt();
 		int[] A = new int[N];
-		ArrayList<Integer> B = new ArrayList<Integer>();
-		ArrayList<Integer> C = new ArrayList<Integer>();
+		int[] B = new int[N];
+		int[] C = new int[N];
+		
+		TreeSet<Integer> aSet = new TreeSet<Integer>();
 		TreeSet<Integer> bSet = new TreeSet<Integer>();
 		TreeSet<Integer> cSet = new TreeSet<Integer>();
 		
 		for (int i = 0; i < N; i++) {
 			A[i] = in.nextInt();
+			aSet.add(A[i]);
 		}
 		for (int i = 0; i < N; i++) {
-			int n = in.nextInt();
-			B.add(n);
-			bSet.add(n);
+			B[i] = in.nextInt();
+			bSet.add(B[i]);
 		}
 		for (int i = 0; i < N; i++) {
-			int n = in.nextInt();
-			C.add(n);
-			cSet.add(n);
+			C[i] = in.nextInt();
+			cSet.add(C[i]);
 		}
-
-		B.sort(null);
-		C.sort(null);
+		
+		Arrays.sort(A);
+		Arrays.sort(B);
+		Arrays.sort(C);
 		
 		long[] temp = new long[N];
 		for (int i = 0; i < N; i++) {
-			Integer c = cSet.higher(B.get(i));
+			Integer c = cSet.higher(B[i]);
+			
 			if (c != null) {
-				int index = C.indexOf(c);
-				temp[i] = N - index;
+				int index = binarySearch2(C, c);
+				temp[i] = (N - index);
 			} else {
 				temp[i] = 0;
 			}
@@ -52,10 +55,46 @@ public class Main {
 			Integer b = bSet.higher(A[i]);
 			
 			if (b != null) {
-				int index = B.indexOf(b);
+				int index = binarySearch2(B, b);
 				answer += temp[index];
 			}
 		}
 		System.out.println(answer);
+	}
+	
+	public static int binarySearch1(int[] arr, int n) {
+		int low = 0;
+		int high = arr.length - 1;
+		
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			
+			if (arr[mid] == n && (mid == arr.length - 1 || arr[mid + 1] != arr[mid])) {
+				return mid;
+			} else if (arr[mid] > n) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return -1;
+	}
+	
+	public static int binarySearch2(int[] arr, int n) {
+		int low = 0;
+		int high = arr.length - 1;
+		
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			
+			if (arr[mid] == n && (mid == 0 || (mid > 0 && arr[mid - 1] != arr[mid]))) {
+				return mid;
+			} else if (arr[mid] < n) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		return -1;
 	}
 }
