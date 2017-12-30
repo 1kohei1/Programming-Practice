37e04d49d9b1406e882fceb6e470ded4e1c79347
Kohei Arai <tobecomebig@gmail.com>
2017-12-30 22:17:42 +0900

    ABC 84-C

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-// ARC 86-D
-// https://beta.atcoder.jp/contests/arc086/tasks/arc086_b
+// ABC 84-C
+// https://beta.atcoder.jp/contests/abc084/tasks/abc084_c
 
 public class Main {
 
@@ -9,63 +9,36 @@ public class Main {
 		Scanner in = new Scanner(System.in);
 		
 		int N = in.nextInt();
-		long[] nums = new long[N];
+		int[] cost = new int[N];
+		int[] start = new int[N];
+		int[] freq = new int[N];
 		
-		int minIndex = 0;
-		int maxIndex = 0;
+		for (int i = 0; i < N - 1; i++) {
+			cost[i] = in.nextInt();
+			start[i] = in.nextInt();
+			freq[i] = in.nextInt();
+		}
+		
+		long[] answer = new long[N];
+		for (int i = 0; i < N - 1; i++) {
+			long sec = start[i] + cost[i];
+			for (int j = i + 1; j < N - 1; j++) {
+				if (sec <= start[j]) {
+					sec = start[j] + cost[j];
+				} else {
+					int n = 1;
+					while (start[j] + n * freq[j] < sec) {
+						n++;
+					}
+					sec = start[j] + n * freq[j];
+					sec += cost[j];
+				}
+			}
+			answer[i] = sec;
+		}
 		
 		for (int i = 0; i < N; i++) {
-			nums[i] = in.nextLong();
-			if (nums[minIndex] > nums[i]) {
-				minIndex = i;
-			}
-			if (nums[maxIndex] < nums[i]) {
-				maxIndex = i;
-			}
-		}
-		
-		int useIndex = -1;
-		boolean isNegative = false;
-		
-		if (Math.abs(nums[minIndex]) < Math.abs(nums[maxIndex])) {
-			useIndex = maxIndex;
-		} else {
-			useIndex = minIndex;
-		}
-
-		if (nums[useIndex] < 0) {
-			isNegative = true;
-		}
-		
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		
-		if (isNegative) {
-			for (int i = N - 1; i >= 1; i--) {
-				while (nums[i - 1] > nums[i]) {
-					nums[i - 1] += nums[useIndex];
-					arr.add(useIndex + 1);
-					arr.add(i - 1 + 1);
-				}
-				if (nums[i - 1] < nums[useIndex]) {
-					useIndex = i - 1;
-				}
-			}
-		} else {
-			for (int i = 0; i < N - 1; i++) {
-				while (nums[i] > nums[i + 1]) {
-					nums[i + 1] += nums[useIndex];
-					arr.add(useIndex + 1);
-					arr.add(i + 1 + 1);
-				}
-				if (nums[useIndex] < nums[i + 1]) {
-					useIndex = i + 1;
-				}
-			}
-		}
-		
-		System.out.println(arr.size() / 2);
-		for (int i = 0; i < arr.size(); i += 2) {
-			System.out.printf("%d %d\n", arr.get(i), arr.get(i + 1));
+			System.out.println(answer[i]);
 		}
 	}
 }
