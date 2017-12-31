78b97aa2afae57f5ee0c5c14cc88b19a726b3052
Kohei Arai <tobecomebig@gmail.com>
2017-12-31 14:36:33 +0900

    ABC 84-D

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,53 @@
 import java.util.*;
 
-// ABC 84-C
-// https://beta.atcoder.jp/contests/abc084/tasks/abc084_c
+// ABC 84-D
+// https://beta.atcoder.jp/contests/abc084/tasks/abc084_d
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		int N = in.nextInt();
-		int[] cost = new int[N];
-		int[] start = new int[N];
-		int[] freq = new int[N];
-		
-		for (int i = 0; i < N - 1; i++) {
-			cost[i] = in.nextInt();
-			start[i] = in.nextInt();
-			freq[i] = in.nextInt();
-		}
-		
-		long[] answer = new long[N];
-		for (int i = 0; i < N - 1; i++) {
-			long sec = start[i] + cost[i];
-			for (int j = i + 1; j < N - 1; j++) {
-				if (sec <= start[j]) {
-					sec = start[j] + cost[j];
-				} else {
-					int n = 1;
-					while (start[j] + n * freq[j] < sec) {
-						n++;
-					}
-					sec = start[j] + n * freq[j];
-					sec += cost[j];
+		int max = 100001;
+		boolean[] isPrime = new boolean[max];
+		Arrays.fill(isPrime, true);
+		isPrime[0] = false;
+		isPrime[1] = false;
+		isPrime[2] = true;
+		for (int i = 2; i <= Math.sqrt(max); i++) {
+			if (i != 2 && i % 2 == 0) {
+				isPrime[i] = false;
+			} else if (isPrime[i] && isPrime(i)) {
+				for (int j = i * 2; j < max; j += i) {
+					isPrime[j] = false;
 				}
 			}
-			answer[i] = sec;
 		}
 		
-		for (int i = 0; i < N; i++) {
-			System.out.println(answer[i]);
+		int[] nums = new int[max];
+		for (int i = 1; i < max; i++) {
+			if (isPrime[i] && isPrime[(i + 1) / 2]) {
+				nums[i] = nums[i - 1] + 1;
+			} else {
+				nums[i] = nums[i - 1];
+			}
 		}
+		
+		int Q = in.nextInt();
+		
+		for (int i = 0; i < Q; i++) {
+			int l = in.nextInt();
+			int r = in.nextInt();
+			System.out.println(nums[r] - nums[l - 1]);
+		}
+	}
+	
+	public static boolean isPrime(int n) {
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
