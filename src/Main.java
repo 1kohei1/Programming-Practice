94a30ed2745f9457b99b3fca0f32c36876075cbd
Kohei Arai <tobecomebig@gmail.com>
2018-01-05 17:47:39 +0900

    ABC 51-D

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,126 @@
 import java.util.*;
 
-// ABC 84-D
-// https://beta.atcoder.jp/contests/abc084/tasks/abc084_d
+// ABC 51-D
+// https://abc051.contest.atcoder.jp/tasks/abc051_d
 
 public class Main {
+
+	static int N;
+	static int[][] map;
+	static int[][] usedEdges;
+	static int numUsedEdges;
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		int max = 100001;
-		boolean[] isPrime = new boolean[max];
-		Arrays.fill(isPrime, true);
-		isPrime[0] = false;
-		isPrime[1] = false;
-		isPrime[2] = true;
-		for (int i = 2; i <= Math.sqrt(max); i++) {
-			if (i != 2 && i % 2 == 0) {
-				isPrime[i] = false;
-			} else if (isPrime[i] && isPrime(i)) {
-				for (int j = i * 2; j < max; j += i) {
-					isPrime[j] = false;
+		N = in.nextInt();
+		int M = in.nextInt();
+		map = new int[N][N];
+		usedEdges = new int[N][N];
+		numUsedEdges = 0;
+		
+		for (int i = 0; i < M; i++) {
+			int a = in.nextInt() - 1;
+			int b = in.nextInt() - 1;
+			int c = in.nextInt();
+			
+			map[a][b] = c;
+			map[b][a] = c;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (i != j) {
+					dijkstra2(i);
 				}
 			}
 		}
-		
-		int[] nums = new int[max];
-		for (int i = 1; i < max; i++) {
-			if (isPrime[i] && isPrime[(i + 1) / 2]) {
-				nums[i] = nums[i - 1] + 1;
-			} else {
-				nums[i] = nums[i - 1];
-			}
-		}
-		
-		int Q = in.nextInt();
-		
-		for (int i = 0; i < Q; i++) {
-			int l = in.nextInt();
-			int r = in.nextInt();
-			System.out.println(nums[r] - nums[l - 1]);
-		}
+		System.out.println(M - numUsedEdges);
 	}
 	
-	public static boolean isPrime(int n) {
-		for (int i = 2; i <= Math.sqrt(n); i++) {
-			if (n % i == 0) {
-				return false;
+	public static void dijkstra2(int start) {
+		int[] dist = new int[N];
+		Arrays.fill(dist, Integer.MAX_VALUE / 2 - 1);
+		dist[start] = 0;
+		
+		ArrayList<ArrayList<Integer>> paths = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i < N; i++) {
+			paths.add(new ArrayList<Integer>());
+		}
+		
+		int[] visited = new int[N];
+		
+		PriorityQueue<Custom_Pair> queue = new PriorityQueue<Custom_Pair>();
+		Custom_Pair p = new Custom_Pair(start, 0);
+		p.path.add(start);
+		queue.add(p);
+		
+		while (queue.size() > 0) {
+			p = queue.poll();
+			
+			if (visited[p.p] == 1) {
+				continue;
+			}
+			
+			for (int i = 0; i < N; i++) {
+				if (map[p.p][i] > 0 && dist[i] > dist[p.p] + map[p.p][i]) {
+					try {
+						dist[i] = dist[p.p] + map[p.p][i];
+						Custom_Pair newPair = (Custom_Pair) p.clone();
+						newPair.p = i;
+						newPair.cost = dist[i];
+						newPair.path.add(i);
+						paths.set(i, newPair.path);
+						queue.add(newPair);
+					} catch (Exception e) {
+						
+					}
+				}
+			}
+			
+			visited[p.p] = 1;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			if (i != start) {
+				ArrayList<Integer> path = paths.get(i);
+				for (int j = 0; j < path.size() - 1; j++) {
+					int a = path.get(j);
+					int b = path.get(j + 1);
+					if (usedEdges[a][b] == 0 && usedEdges[b][a] == 0) {
+						usedEdges[a][b] = 1;
+						usedEdges[b][a] = 1;
+						numUsedEdges++;
+					}
+				}
 			}
 		}
-		return true;
+	}
+}
+
+class Custom_Pair implements Comparable<Custom_Pair> {
+	int p;
+	int cost;
+	ArrayList<Integer> path;
+	
+	public Custom_Pair(int p, int cost) {
+		super();
+		this.p = p;
+		this.cost = cost;
+		this.path = new ArrayList<Integer>();
+	}
+
+	@Override
+	public int compareTo(Custom_Pair o) {
+		return this.cost - o.cost;
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		Custom_Pair p = new Custom_Pair(this.p, this.cost);
+		for (int i = 0; i < this.path.size(); i++) {
+			p.path.add(this.path.get(i));
+		}
+		return p;
 	}
 }
