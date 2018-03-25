532082ae256bcab2c3a90f7f55fcaa4ac1b1958d
Kohei Arai <tobecomebig@gmail.com>
2018-03-25 18:13:20 -0400

    ARC 93-D

--- src/Main.java
+++ src/Main.java
@@ -1,96 +1,52 @@
 import java.util.*;
 
-// ARC 90-D
-// https://beta.atcoder.jp/contests/arc090/tasks/arc090_b
+// ARC 93-D
+// https://beta.atcoder.jp/contests/arc093/tasks/arc093_b
 
 public class Main {
-
-	static int N;
-	static HashMap<Integer, HashMap<Integer, Integer>> edges;
-	static int[] values;
-	static boolean[] visited;
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-
-		N = in.nextInt();
-		int M = in.nextInt();
 		
-		edges = new HashMap<Integer, HashMap<Integer, Integer>>();
-		Input[] inputs = new Input[M];
+		int numWhite = in.nextInt();
+		int numBlack = in.nextInt();
 		
-		for (int i = 0; i < M; i++) {
-			int l = in.nextInt();
-			int r = in.nextInt();
-			int c = in.nextInt();
-			
-			if (edges.containsKey(l)) {
-				HashMap<Integer, Integer> e = edges.get(l);
-				e.put(r, c);
-			} else {
-				HashMap<Integer, Integer> e = new HashMap<Integer, Integer>();
-				e.put(r, c);
-				edges.put(l, e);
-			}
-			
-			if (edges.containsKey(r)) {
-				HashMap<Integer, Integer> e = edges.get(r);
-				e.put(l, -1 * c);
-			} else {
-				HashMap<Integer, Integer> e = new HashMap<Integer, Integer>();
-				e.put(l, -1 * c);
-				edges.put(r, e);
-			}
-			
-			inputs[i] = new Input(l, r, c);
-		}
+		char WHITE = '.';
+		char BLACK = '#';
 		
-		values = new int[N + 1];
-		visited = new boolean[N + 1];
+		char[][] map = new char[100][100];
 		
-		for (int i = 1; i < N + 1; i++) {
-			if (visited[i] == false) {
-				dfs(i, 0);
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				// Left half is white and right half is black
+				map[i][j] = j <= 49 ? WHITE : BLACK;
 			}
 		}
 		
-		boolean answer = true;
-		
-		for (int i = 0; i < M && answer; i++) {
-			if (values[inputs[i].l] + inputs[i].c != values[inputs[i].r]) {
-				answer = false;
+		int count = 1;
+		// Fill black in white canvas
+		for (int i = 0; i < 100; i += 2) {
+			for (int j = 0; j <= 49 && count < numBlack; j += 2) {
+				map[i][j] = BLACK;
+				count++;
 			}
 		}
 		
-		System.out.println(answer ? "Yes" : "No");
-	}
-	
-	public static void dfs(int n, int val) {
-		visited[n] = true;
-		values[n] = val;
-		
-		HashMap<Integer, Integer> e = edges.get(n);
-		
-		if (e == null) {
-			return;
-		}
-		
-		for (int next : e.keySet()) {
-			if (visited[next] == false) {
-				dfs(next, val + e.get(next));
+		count = 1;
+		// Fill white in black canvas
+		for (int i = 0; i < 100; i += 2) {
+			for (int j = 51; j < 100 && count < numWhite; j += 2) {
+				map[i][j] = WHITE;
+				count++;
 			}
 		}
-	}
-}
-
-class Input {
-	int l;
-	int r;
-	int c;
-	
-	public Input(int l, int r, int c) {
-		this.l = l;
-		this.r = r;
-		this.c = c;
+		
+		System.out.println("100 100");
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				System.out.printf("%c", map[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }
