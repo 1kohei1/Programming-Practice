53e5659eb26d20c1a589f98c4d58e7d8d542ff6c
Kohei Arai <tobecomebig@gmail.com>
2018-04-01 05:49:55 -0700

    ABC 57-D

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,73 @@
 import java.util.*;
 
-// ABC 76-C
-// https://beta.atcoder.jp/contests/abc076/tasks/abc076_c
+// ABC 57-D
+// https://beta.atcoder.jp/contests/abc057/tasks/abc057_d
 
 public class Main {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-
-		char[] S = in.next().toCharArray();
-		char[] T = in.next().toCharArray();
 		
-		ArrayList<String> answers = new ArrayList<String>();
+		int N = in.nextInt();
+		int A = in.nextInt();
+		int B = in.nextInt();
 		
-		for (int i = 0; i < S.length; i++) {
-			if (i + T.length > S.length) {
-				continue;
+		long[] nums = new long[N];
+		for (int i = 0; i < N; i++) {
+			nums[i] = in.nextLong();
+		}
+		Arrays.sort(nums);
+		
+		long sum = 0;
+		boolean isAllSame = true;
+		
+		for (int i = N - 1; i >= N - A; i--) {
+			sum += nums[i];
+			if (i != N - 1 && nums[i] != nums[i + 1]) {
+				isAllSame = false;
 			}
-			
-			char[] answer = S.clone();
-			
-			for (int j = 0; j < i; j++) {
-				if (answer[j] == '?') {
-					answer[j] = 'a';
+		}
+		
+		double avg = ((double) sum) / A;
+		
+		long lowest = nums[N - A];
+		int numLowestInNums = 0;
+		int numLowestInAvg = 0;
+		
+		for (int i = 0; i < N; i++) {
+			if (nums[i] == lowest) {
+				numLowestInNums++;
+				if (i >= N - A) {
+					numLowestInAvg++;
 				}
 			}
-			
-			boolean isAnswer = true;
-			
-			for (int j = 0; j < T.length; j++) {
-				int index = i + j;
-				
-				if (answer[index] == '?' || answer[index] == T[j]) {
-					answer[index] = T[j];
+		}
+		
+		long count = comb(numLowestInNums, numLowestInAvg);
+		
+		if (isAllSame) {
+			for (int i = N - A - 1; i >= N - B; i--) {
+				if (nums[i] == lowest) {
+					numLowestInAvg += 1;
+					count += comb(numLowestInNums, numLowestInAvg);
 				} else {
-					isAnswer = false;
+					break;
 				}
-			}
-			
-			for (int j = i + T.length; j < S.length; j++) {
-				if (answer[j] == '?') {
-					answer[j] = 'a';
-				}
-			}
-			
-			if (isAnswer) {
-				answers.add(new String(answer));
 			}
 		}
 		
-		answers.sort(null);
+		System.out.printf("%.6f\n", avg);
+		System.out.println(count);
+	}
+	
+	public static long comb(int n, int r) {
+		long v = 1;
 		
-		if (answers.size() > 0) {
-			System.out.println(answers.get(0));
-		} else {
-			System.out.println("UNRESTORABLE");
+		for (int i = 0; i < r; i++) {
+			v *= n - i;
+			v /= (i + 1);
 		}
+		
+		return v;
 	}
 }
