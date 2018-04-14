08fc41eb198c1f101d18e739ec1a0fb99329ff0f
Kohei Arai <tobecomebig@gmail.com>
2018-04-14 07:42:07 -0700

    ARC 95-D

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,31 @@
 import java.util.*;
 
-// ABC 57-D
-// https://beta.atcoder.jp/contests/abc057/tasks/abc057_d
+// ARC 95-D
+// https://beta.atcoder.jp/contests/arc095/tasks/arc095_b
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
+
 		int N = in.nextInt();
-		int A = in.nextInt();
-		int B = in.nextInt();
-		
-		long[] nums = new long[N];
+
+		int[] nums = new int[N];
 		for (int i = 0; i < N; i++) {
-			nums[i] = in.nextLong();
+			nums[i] = in.nextInt();
 		}
+		
 		Arrays.sort(nums);
-		
-		long sum = 0;
-		boolean isAllSame = true;
-		
-		for (int i = N - 1; i >= N - A; i--) {
-			sum += nums[i];
-			if (i != N - 1 && nums[i] != nums[i + 1]) {
-				isAllSame = false;
-			}
-		}
-		
-		double avg = ((double) sum) / A;
-		
-		long lowest = nums[N - A];
-		int numLowestInNums = 0;
-		int numLowestInAvg = 0;
-		
-		for (int i = 0; i < N; i++) {
-			if (nums[i] == lowest) {
-				numLowestInNums++;
-				if (i >= N - A) {
-					numLowestInAvg++;
-				}
-			}
-		}
-		
-		long count = comb(numLowestInNums, numLowestInAvg);
-		
-		if (isAllSame) {
-			for (int i = N - A - 1; i >= N - B; i--) {
-				if (nums[i] == lowest) {
-					numLowestInAvg += 1;
-					count += comb(numLowestInNums, numLowestInAvg);
-				} else {
-					break;
-				}
-			}
-		}
-		
-		System.out.printf("%.6f\n", avg);
-		System.out.println(count);
-	}
 	
-	public static long comb(int n, int r) {
-		long v = 1;
+		int n = nums[N - 1];
+		int r = nums[0];
 		
-		for (int i = 0; i < r; i++) {
-			v *= n - i;
-			v /= (i + 1);
+		for (int i = 1; i < N; i++) {
+			if (Math.abs(nums[i] - n / 2.0) < Math.abs(r - n / 2.0)) {
+				r = nums[i];
+			}
 		}
 		
-		return v;
+		System.out.printf("%d %d\n", n, r);
 	}
 }
