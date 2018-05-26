5e5cacf0ed488a545e61039e6d04a25b3aeaf95e
Kohei Arai <tobecomebig@gmail.com>
2018-05-26 11:22:21 -0700

    ARC 98-C

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-// ABC 70-D
-// https://beta.atcoder.jp/contests/abc070/tasks/abc070_d
+// ARC 98-C
+// https://beta.atcoder.jp/contests/arc098/tasks/arc098_a
 
 public class Main {
 
@@ -9,52 +9,56 @@ public class Main {
 		Scanner in = new Scanner(System.in);
 
 		int N = in.nextInt();
+		char[] c = in.next().toCharArray();
 		
-		ArrayList<ArrayList<KPoint>> map = new ArrayList<ArrayList<KPoint>>();
+		int[][] arr1 = new int[N][2];
+		int[][] arr2 = new int[N][2];
+		
 		for (int i = 0; i < N; i++) {
-			map.add(new ArrayList<KPoint>());
-		}
-		
-		for (int i = 0; i < N - 1; i++) {
-			int a = in.nextInt() - 1;
-			int b = in.nextInt() - 1;
-			int c = in.nextInt();
-
-			map.get(a).add(new KPoint(b, c));
-			map.get(b).add(new KPoint(a, c));
-		}
-		int Q = in.nextInt();
-		int K = in.nextInt() - 1;
-
-		long[] dist = new long[N];
-		int[] visited = new int[N];
-		dfs(K, dist, visited, 0, map);
-		
-		for (int i = 0; i < Q; i++) {
-			int start = in.nextInt() - 1;
-			int end = in.nextInt() - 1;
-			System.out.println(dist[start] + dist[end]);
-		}
-	}
-	
-	public static void dfs(int curr, long[] dist, int[] visited, long d, ArrayList<ArrayList<KPoint>> map) {
-		visited[curr] = 1;
-		dist[curr] = d;
-		for (int i = 0; i < map.get(curr).size(); i++) {
-			KPoint kp = map.get(curr).get(i);
-			if (visited[kp.p] == 0) {
-				dfs(kp.p, dist, visited, d + kp.dist, map);
+			if (c[i] == 'W') {
+				if (i == 0) {
+					arr1[i][0]++;	
+				} else {
+					arr1[i][0] = arr1[i - 1][0] + 1;
+					arr1[i][1] = arr1[i - 1][1];
+				}
+			} else {
+				if (i == 0) {
+					arr1[i][1]++;	
+				} else {
+					arr1[i][0] = arr1[i - 1][0];
+					arr1[i][1] = arr1[i - 1][1] + 1;
+				}
 			}
 		}
-	}
-}
-
-class KPoint {
-	int p;
-	int dist;
-	
-	public KPoint(int p, int dist) {
-		this.p = p;
-		this.dist = dist;
+		
+		for (int i = N - 1; i >= 0; i--) {
+			if (c[i] == 'W') {
+				if (i == N - 1) {
+					arr2[i][0]++;
+				} else {
+					arr2[i][0] = arr2[i + 1][0] + 1;
+					arr2[i][1] = arr2[i + 1][1]; 
+				}
+			} else {
+				if (i == N - 1) {
+					arr2[i][1]++;
+				} else {
+					arr2[i][0] = arr2[i + 1][0];
+					arr2[i][1] = arr2[i + 1][1] + 1; 
+				}
+			}
+		}
+		
+		int answer = N;
+		
+		for (int i = 0; i < N; i++) {
+			int left = i == 0 ? 0 : arr1[i - 1][0];
+			int right = i == N - 1 ? 0 : arr2[i + 1][1];
+			
+			answer = Math.min(answer, left + right);
+		}
+		
+		System.out.println(answer);
 	}
 }
