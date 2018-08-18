b01053e6c860c1d822765bed5a0207e6aaf70649
Kohei Arai <tobecomebig@gmail.com>
2018-08-18 10:36:29 -0700

    ABC 106-D

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,41 @@
 import java.util.*;
 
-// ARC 98-D
-// https://beta.atcoder.jp/contests/arc098/tasks/arc098_b
+// ARC 106-D
+// https://beta.atcoder.jp/contests/abc106/tasks/abc106_d
 
 public class Main {
-
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 
-		int N = in.nextInt();
-		long[] arr = new long[N];
-		
-		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextLong();
-		}
-		
-		long answer = 0;
-		long temp = 0;
-		int left = 0;
-		int right = 0;
-		
-		while (left < N) {
-			if (left == right) {
-				temp = arr[right];
-				answer++;
-				right++;
-			} else if (right < N && (temp & arr[right]) == 0) {
-				temp += arr[right];
-				answer++;
-				right++;
-			} else {
-				temp -= arr[left];
-				left++;
-//				int num = right - left;
-//				answer += num * (num - 1) / 2;
-				answer += right - left;
-			}
-		}
-		
-//		answer += N;
-		
-		System.out.println(answer);
-	}
+      int N = in.nextInt();
+      int M = in.nextInt();
+      int Q = in.nextInt();
+
+      int[][] map = new int[N + 1][N + 1];
+      for (int i = 0; i < M; i++) {
+        int L = in.nextInt();
+        int R = in.nextInt();
+
+        map[L][R] += 1;
+      }
+
+      for (int i = 0; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+          map[i][j] += map[i][j - 1];
+        }
+      }
+      
+      for (int i = 0; i < Q; i++) {
+        int P = in.nextInt();
+        int q = in.nextInt();
+
+        int answer = 0;
+        for (int j = P; j <= q; j++) {
+          answer += map[j][q];
+        }
+
+        System.out.println(answer);
+      }
+    }
 }
