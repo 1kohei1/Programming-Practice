c0c4b45b1d604dd3b8b92b6761416476108f389c
Kohei Arai <tobecomebig@gmail.com>
2018-10-14 02:29:25 -0700

    AGC 28-A

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-// ABC 112-D
-// https://beta.atcoder.jp/contests/abc112/tasks/abc112_d
+// AGC 28-A
+// https://beta.atcoder.jp/contests/agc028/tasks/agc028_a
 
 public class Main {
 
@@ -10,21 +10,96 @@ public class Main {
 		
 		int N = in.nextInt();
 		int M = in.nextInt();
-		int answer = 1;
+		String S = in.next();
+		String T = in.next();
 		
-		for (int i = (int) Math.sqrt(M); i > 0; i--) {
-			if (M % i == 0) {
-				int other = M / i;
-				if (M / other >= N) {
-					answer = Math.max(answer, other);
-				}
-				if (M / i >= N) {
-					answer = Math.max(answer, i);
-				}
+		long gcd = gcd(N, M);
+		long lcm = lcm(N, M);
+
+		boolean isPossible = true;
+		for (int i = 0; i < gcd; i++) {
+			int index1 = (int) (i * (lcm / M));
+			int index2 = (int) (i * (lcm / N));
+			
+			if (S.charAt(index1) != T.charAt(index2)) {
+				isPossible = false;
 			}
 		}
-		System.out.println(answer);
+		
+		if (isPossible) {
+			System.out.println(lcm);
+		} else {
+			System.out.println(-1);
+		}
 	}
+	
+	// Taken from https://www.geeksforgeeks.org/c-program-find-gcd-hcf-two-numbers/
+	public static long gcd(long a, long b) {
+		if (b == 0) {
+			return a;
+		}
+		return gcd(b, a % b);
+	}
+	
+	public static long lcm(long a, long b) {
+		return a * b / gcd(a, b);
+	}
+	
+    public static List<List<Integer>> threeSum(int[] nums) {
+    	Arrays.sort(nums);
+    	
+    	List<List<Integer>> answers = new LinkedList<>();
+    	HashMap<Integer, ArrayList<Integer>> memo = new HashMap<Integer, ArrayList<Integer>>();
+    	HashMap<String, Integer> uniqueAnswers = new HashMap<String, Integer>();
+    	
+    	for (int i = 0; i < nums.length; i++) {
+    		int val = nums[i];
+    		if (!memo.containsKey(val)) {
+    			memo.put(val, new ArrayList<Integer>());
+    		}
+    		memo.get(val).add(i);
+    	}
+    	
+    	int low = 0;
+    	int high = nums.length - 1;
+    	while (high - low > 1) {
+    		int rem = 0 - nums[low] - nums[high];
+    		// Check if rem exists in the nums
+    		if (memo.containsKey(rem)) {
+    			String str = "" + nums[low] + "," + rem + "," + nums[high];
+    			// Check if it is unique answer
+    			if (!uniqueAnswers.containsKey(str)) {
+    				// Check if there is no duplicate index used
+    				ArrayList<Integer> arr = memo.get(rem);
+    				int len = arr.size();
+    				for (int i = 0; i < arr.size(); i++) {
+    					int val = arr.get(i);
+    					if (val == low || val == high) {
+    						len--;
+    					}
+    				}
+    				
+    				// It's an answer. Now, add it to answers and uniqueAnswers
+    				if (len > 0) {
+    					ArrayList<Integer> arr2 = new ArrayList<Integer>();
+    					arr2.add(nums[low]);
+    					arr2.add(rem);
+    					arr2.add(nums[high]);
+    					answers.add(arr2);
+        				uniqueAnswers.put(str, 1);
+    				}
+    			}
+    			low++;
+    		} else if (0 - nums[low] - nums[high] > 0) {
+    			low++;
+    		} else {
+    			high--;
+    		}
+    	}
+    	
+    	
+    	return answers;
+    }
 }
 
 class TreeNode {
